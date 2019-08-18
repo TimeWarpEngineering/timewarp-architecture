f9f2fcb1720695ecc6cf564b0c2010692289a99f
Steven T. Cramer <Steve.Cramer@TimeWarp.Enterprises>
2019-08-18 07:11:25 +0000

    Clean up the TestServer and test Fixture

--- Source/Blazor.Hosted/content/TimeWarp.BlazorHosted-CSharp/Tests/BlazorHosted-CSharp.Server.Integration.Tests/Infrastructure/BlazorStateTestServer.cs
+++ Source/Blazor.Hosted/content/TimeWarp.BlazorHosted-CSharp/Tests/BlazorHosted-CSharp.Server.Integration.Tests/Infrastructure/BlazorStateTestServer.cs
@@ -10,7 +10,7 @@
 
     private static IWebHostBuilder WebHostBuilder() =>
       WebHost.CreateDefaultBuilder()
-      .UseStartup<Startup>()
+      .UseStartup<Server.Startup>()
       .UseEnvironment("Local");
   }
 }
