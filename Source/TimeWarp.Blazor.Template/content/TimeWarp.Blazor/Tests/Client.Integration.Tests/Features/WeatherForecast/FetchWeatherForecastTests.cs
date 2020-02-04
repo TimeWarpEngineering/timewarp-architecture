7d1abcd8c796be8cfca69cb21c3ae0006e3a5063
StevenTCramer <Steven.Cramer@CineMassive.com>
2020-02-04 04:45:05 +0000

    This migrates the FetchWeatherForecastTestsand they pass
    Others still failing.

--- Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Tests/Client.Integration.Tests/Features/WeatherForecast/FetchWeatherForecastTests.cs
+++ Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Tests/Client.Integration.Tests/Features/WeatherForecast/FetchWeatherForecastTests.cs
@@ -1,34 +1,23 @@
-﻿namespace TimeWarp.Blazor.Client.Integration.Tests.Features.WeatherForecast
+namespace TimeWarp.Blazor.Client.Integration.Tests.Features.WeatherForecast
 {
-  using BlazorState;
-  using MediatR;
-  using Microsoft.Extensions.DependencyInjection;
+  using Microsoft.AspNetCore.Blazor.Hosting;
   using Shouldly;
-  using System;
   using System.Threading.Tasks;
   using TimeWarp.Blazor.Client.Integration.Tests.Infrastructure;
   using TimeWarp.Blazor.Client.WeatherForecastFeature;
   using static TimeWarp.Blazor.Client.WeatherForecastFeature.WeatherForecastsState;
 
-  internal class FetchWeatherForecastTests
+  internal class FetchWeatherForecastTests : BaseTest
   {
-    private readonly IMediator Mediator;
-    private readonly IServiceProvider ServiceProvider;
-    private readonly IStore Store;
     private WeatherForecastsState WeatherForecastsState => Store.GetState<WeatherForecastsState>();
 
-    public FetchWeatherForecastTests(TestFixture aTestFixture)
-    {
-      ServiceProvider = aTestFixture.ServiceProvider;
-      Mediator = ServiceProvider.GetService<IMediator>();
-      Store = ServiceProvider.GetService<IStore>();
-    }
+    public FetchWeatherForecastTests(IWebAssemblyHost aWebAssemblyHost) : base(aWebAssemblyHost) { }
 
     public async Task Should_Fetch_WeatherForecasts()
     {
       var fetchWeatherForecastsRequest = new FetchWeatherForecastsAction();
 
-      _ = await Mediator.Send(fetchWeatherForecastsRequest);
+      await Send(fetchWeatherForecastsRequest);
 
       WeatherForecastsState.WeatherForecasts.Count.ShouldBeGreaterThan(0);
     }
