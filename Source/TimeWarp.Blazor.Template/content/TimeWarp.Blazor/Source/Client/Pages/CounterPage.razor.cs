516a120f0c5f2c70308490314edc291333a5dd98
StevenTCramer <Steven.Cramer@CineMassive.com>
2020-02-05 03:27:42 +0000

    Use Partial classes
    Update Counter Page to support testing Javascript. Based on the code in Blazor-State test App.

--- Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Source/Client/Pages/CounterPage.razor.cs
+++ Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Source/Client/Pages/CounterPage.razor.cs
@@ -1,9 +1,14 @@
-﻿namespace TimeWarp.Blazor.Client.Pages
+namespace TimeWarp.Blazor.Client.Pages
 {
+  using BlazorState.Features.Routing;
+  using System.Threading.Tasks;
   using TimeWarp.Blazor.Client.BaseFeature;
 
-  public class CounterPageBase : BaseComponent
+  public partial class CounterPage: BaseComponent
   {
     public const string Route = "/counter";
+
+    protected async Task ButtonClick() =>
+      _ = await Mediator.Send(new RouteState.ChangeRouteAction { NewRoute = "/" });
   }
 }
