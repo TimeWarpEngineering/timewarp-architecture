7c8283ba9097da1b472b462c996801b39534fdaf
StevenTCramer <Steven.Cramer@CineMassive.com>
2020-02-08 07:19:30 +0000

    UseHttpSys
    This option isn't to difficult to configure.  Although normally would use Kestral.

--- Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Source/Server/Program.cs
+++ Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Source/Server/Program.cs
@@ -1,13 +1,38 @@
-﻿namespace TimeWarp.Blazor.Server
+namespace TimeWarp.Blazor.Server
 {
   using Microsoft.AspNetCore.Hosting;
+
+#if UseHttpSys
+  using Microsoft.AspNetCore.Server.HttpSys;
+#endif
+
   using Microsoft.Extensions.Hosting;
 
   public class Program
   {
     public static IHostBuilder CreateHostBuilder(string[] aArgumentArray) =>
       Host.CreateDefaultBuilder(aArgumentArray)
-        .ConfigureWebHostDefaults(aWebHostBuilder => aWebHostBuilder.UseStartup<Startup>());
+        .ConfigureWebHostDefaults
+        (
+          aWebHostBuilder =>
+          {
+#if UseHttpSys
+            aWebHostBuilder.UseHttpSys
+            (
+              options =>
+              {
+                options.AllowSynchronousIO = false;
+                options.Authentication.Schemes = AuthenticationSchemes.None;
+                options.Authentication.AllowAnonymous = true;
+                options.MaxConnections = null;
+                options.MaxRequestBodySize = 30000000;
+                options.UrlPrefixes.Add("http://localhost:5005");
+              }
+            );
+#endif
+            aWebHostBuilder.UseStartup<Startup>();
+          }
+        );
 
     public static void Main(string[] aArgumentArray) => CreateHostBuilder(aArgumentArray).Build().Run();
   }
