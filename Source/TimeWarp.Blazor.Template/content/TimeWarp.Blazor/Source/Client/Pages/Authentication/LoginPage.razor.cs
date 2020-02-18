be20da7db2ea65f8f528260e5553806419d68f35
StevenTCramer <Steven.Cramer@CineMassive.com>
2020-02-18 08:07:27 +0000

    Use BaseComponent vs ComponentBase for consistency.

--- Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Source/Client/Pages/Authentication/LoginPage.razor.cs
+++ Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Source/Client/Pages/Authentication/LoginPage.razor.cs
@@ -1,8 +1,8 @@
 namespace TimeWarp.Blazor.Pages
 {
-  using Microsoft.AspNetCore.Components;
+  using TimeWarp.Blazor.Features.Bases.Client;
 
-  public partial class LoginPage : ComponentBase
+  public partial class LoginPage : BaseComponent
   {
     public const string Route = "Login";
   }
