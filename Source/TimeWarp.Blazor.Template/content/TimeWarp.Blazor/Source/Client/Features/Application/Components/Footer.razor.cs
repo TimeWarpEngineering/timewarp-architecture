748f2b1512055af0a926f79be4fc7ed91fe1f00c
StevenTCramer <Steven.Cramer@CineMassive.com>
2020-02-18 07:49:02 +0000

    Add BaseComponent
    Code generation will work given partial classe but IDE complains.
    This solves that.

--- Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Source/Client/Features/Application/Components/Footer.razor.cs
+++ Source/TimeWarp.Blazor.Template/content/TimeWarp.Blazor/Source/Client/Features/Application/Components/Footer.razor.cs
@@ -1,6 +1,8 @@
+using TimeWarp.Blazor.Features.Bases.Client;
+
 namespace TimeWarp.Blazor.Features.Applications.Components
 {
-  public partial class Footer
+  public partial class Footer: BaseComponent
   {
     protected string Version => ApplicationState.Version;
   }
