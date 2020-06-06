e4f012dd160f2a12f90707e409db057d20cad31a
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-06 09:11:45 +0000

    Add FeatureAnnotations
    This allows us to define contants at the Feature level vs just the Endpoint level

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Server/Features/WeatherForecast/Get/GetWeatherForecastsEndpoint.cs
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Server/Features/WeatherForecast/Get/GetWeatherForecastsEndpoint.cs
@@ -1,12 +1,14 @@
 namespace TimeWarp.Blazor.Features.WeatherForecasts
 {
   using Microsoft.AspNetCore.Mvc;
+  using Swashbuckle.AspNetCore.Annotations;
   using System.Threading.Tasks;
   using TimeWarp.Blazor.Features.Bases;
 
   public class GetWeatherForecastsEndpoint : BaseEndpoint<GetWeatherForecastsRequest, GetWeatherForecastsResponse>
   {
     [HttpGet(GetWeatherForecastsRequest.Route)]
+    [SwaggerOperation(Tags = new[] { FeatureAnnotations.FeatureGroup })]
     public async Task<IActionResult> Process(GetWeatherForecastsRequest aGetWeatherForecastsRequest) => await Send(aGetWeatherForecastsRequest);
   }
 }
