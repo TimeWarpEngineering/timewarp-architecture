481783683a119be5d39b2c05c5d01ba0f9ef381a
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-07 14:05:13 +0000

    Trying to write Integration test to prove Validation is wired up correctly.

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Tests/Server.Integration.Tests/Features/WeatherForecast/Get/GetWeatherForecastsEndpoint_Tests.cs
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Tests/Server.Integration.Tests/Features/WeatherForecast/Get/GetWeatherForecastsEndpoint_Tests.cs
@@ -1,13 +1,17 @@
 ﻿namespace GetWeatherForecastsEndpoint
 {
   using FluentAssertions;
+  using Microsoft.AspNetCore.Mvc;
   using Microsoft.AspNetCore.Mvc.Testing;
   using Shouldly;
+  using System;
+  using System.Net;
+  using System.Net.Http;
   using System.Text.Json;
   using System.Threading.Tasks;
   using TimeWarp.Blazor.Features.WeatherForecasts;
-  using TimeWarp.Blazor.Server.Integration.Tests.Infrastructure;
   using TimeWarp.Blazor.Server;
+  using TimeWarp.Blazor.Server.Integration.Tests.Infrastructure;
 
   public class Returns : BaseTest
   {
@@ -30,6 +34,24 @@
       ValidateGetWeatherForecastsResponse(getWeatherForecastsResponse);
     }
 
+    public async Task Returns_ValidationErrror()
+    {
+      GetWeatherForecastsRequest.Days = -1;
+
+      HttpResponseMessage httpResponseMessage = await HttpClient.GetAsync(GetWeatherForecastsRequest.RouteFactory);
+
+      string json = await httpResponseMessage.Content.ReadAsStringAsync();
+
+      //BadRequestObjectResult response = JsonSerializer.Deserialize<ModelState>(json, JsonSerializerOptions);
+
+      Console.WriteLine("=================================");
+      Console.WriteLine(json);
+      Console.WriteLine("=================================");
+      httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+      
+
+    }
+
     private void ValidateGetWeatherForecastsResponse(GetWeatherForecastsResponse aGetWeatherForecastsResponse)
     {
       aGetWeatherForecastsResponse.RequestId.ShouldBe(GetWeatherForecastsRequest.Id);
@@ -37,6 +59,5 @@
       aGetWeatherForecastsResponse.WeatherForecasts.Count.ShouldBe(GetWeatherForecastsRequest.Days);
       aGetWeatherForecastsResponse.WeatherForecasts.Count.Should().Be(GetWeatherForecastsRequest.Days);
     }
-
   }
 }
