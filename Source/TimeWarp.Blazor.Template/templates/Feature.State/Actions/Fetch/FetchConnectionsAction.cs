ae8fe0fd5b6cb509a144af113db81306a90e38c6
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-22 05:50:24 +0000

    Start on State template

---
+++ Source/TimeWarp.Blazor.Template/templates/Feature.State/Actions/Fetch/FetchConnectionsAction.cs
@@ -0,0 +1,9 @@
+﻿namespace __RootNamespace__.Features.__FeatureName__s
+{
+  using __RootNamespace__.Features.Bases;
+
+  internal partial class __FeatureName__State
+  {
+    public class Fetch__FeatureName__sAction : BaseAction { }
+  }
+}
---
+++ Source/TimeWarp.Blazor.Template/templates/Feature.State/__FeatureName__State.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorHosted.Features.__FeatureName__
+{
+  using BlazorState;
+  using System.Collections.Generic;
+
+  public partial class __FeatureName__State : State<__FeatureName__State>
+  {
+    public List<__FeatureName__Dto> ___FeatureName__s { get; set; } = null!;
+
+    public IReadOnlyList<__FeatureName__Dto> __FeatureName__s => ___FeatureName__Records.AsReadOnly();
+    public __FeatureName__State() 
+    {
+      ___FeatureName__Records = new List<__FeatureName__Dto>();
+    }
+
+    public override void Initialize() 
+    {
+      ___FeatureName__s.Clear();
+    }
+
+  }
+}
