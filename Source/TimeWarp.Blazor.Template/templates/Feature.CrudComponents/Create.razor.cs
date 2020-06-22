4a78d99a3d5ff8ec4b9b83e43bed8d41637850ab
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-22 05:49:40 +0000

    Start on a CrudPages template

---
+++ Source/TimeWarp.Blazor.Template/templates/Feature.CrudComponents/Create.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace eShopOnBlazorWasm.Features.CatalogItems.Components
+{
+  using eShopOnBlazorWasm.Features.Bases;
+  using eShopOnBlazorWasm.Features.CatalogBrands;
+  using eShopOnBlazorWasm.Features.CatalogTypes;
+  using Microsoft.AspNetCore.Components;
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using static BlazorState.Features.Routing.RouteState;
+  using static eShopOnBlazorWasm.Features.CatalogItems.CatalogItemState;
+
+  public partial class Create: BaseComponent
+  {
+    public CreateCatalogItemRequest CreateCatalogItemRequest { get; set; }
+
+    private IReadOnlyList<CatalogBrandDto> CatalogBrands => CatalogBrandState.CatalogBrandsAsList;
+    private IReadOnlyList<CatalogTypeDto> CatalogTypes => CatalogTypeState.CatalogTypesAsList;
+
+    [Parameter] public string RedirectRoute { get; set; }
+
+    protected override Task OnInitializedAsync()
+    {
+      CreateCatalogItemRequest = new CreateCatalogItemRequest();
+      
+      return base.OnInitializedAsync();
+    }
+
+    protected async Task HandleValidSubmit()
+    {
+      _ = await Mediator.Send(new CreateCatalogItemAction { CreateCatalogItemRequest = CreateCatalogItemRequest });
+      _ = await Mediator.Send(new ChangeRouteAction { NewRoute = RedirectRoute });
+    }
+  }
+}
