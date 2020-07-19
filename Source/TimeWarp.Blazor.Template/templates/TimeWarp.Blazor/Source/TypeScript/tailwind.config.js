59d21b152469c1a3dd148b1ddd92455d1476aace
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-07-19 15:04:08 +0000

    Add Tailwinds configuration using InterFont

---
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/TypeScript/tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui'),
+  ],
+}
