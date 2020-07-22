7f545dc947859937e2e8fb32526eaba497d56930
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-07-22 03:38:59 +0000

    enable hover for borderWidth and set functional color names
    based the color names on bootstrap

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/TypeScript/tailwind.config.js
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/TypeScript/tailwind.config.js
@@ -6,12 +6,22 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        primary: defaultTheme.colors.blue,
+        secondary: defaultTheme.colors.gray,
+        accent: defaultTheme.colors.teal,
+        danger: defaultTheme.colors.red,
+        warning: defaultTheme.colors.yellow,
+        positive: defaultTheme.colors.green,
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  variants: {},
+  variants: {
+    borderWidth: ['hover'],
+  },
   plugins: [
     require('@tailwindcss/ui'),
   ],
