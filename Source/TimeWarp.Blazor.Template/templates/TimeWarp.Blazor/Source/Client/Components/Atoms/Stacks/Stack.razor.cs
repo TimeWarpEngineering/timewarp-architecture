dbe1d0b3c0385a9f6ad91cc96faa0938f788cb3a
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-07-24 04:32:31 +0000

    Start Work on Stack Components

---
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Components/Atoms/Stacks/Stack.razor.cs
@@ -0,0 +1,44 @@
+﻿namespace TimeWarp.Blazor.Components
+{
+  using BlazorComponentUtilities;
+  using Microsoft.AspNetCore.Components;
+  using System;
+  public partial class Stack: ParentComponent
+  {
+    private readonly string BaseCss = "flex";
+
+    private readonly string HorizontalCss = "flex-row";
+
+    private readonly string VerticalCss = string.Join
+    (
+      separator: " ",
+      "flex-col"
+    );
+
+    [Parameter] public StackVariant Variant { get; set; } = StackVariant.Horizontal;
+
+    [Parameter] public bool Wrap { get; set; }
+
+    private string CssClass { get; set; }
+
+    public enum StackVariant
+    {
+      Horizontal,
+      Vertical
+    }
+
+    protected override void OnParametersSet()
+    {
+      Console.WriteLine("OnParametersSet.1");
+      string cssString = Variant == StackVariant.Horizontal ? HorizontalCss : VerticalCss;
+      cssString = BaseCss + " " + cssString;
+      CssClass =
+        new CssBuilder(cssString)
+        .AddClass("flex-wrap", Wrap)
+        .AddClassFromAttributes(Attributes)
+        .Build();
+
+      base.OnParametersSet();
+    }
+  }
+}
---
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Components/Base/ParentComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeWarp.Blazor.Components
+{
+  using Microsoft.AspNetCore.Components;
+
+  public class ParentComponent : DisplayComponent
+  {
+    [Parameter] public RenderFragment ChildContent { get; set; }
+  }
+}
