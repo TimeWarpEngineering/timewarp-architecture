efa1303f02c799a5707c103c9d6d35140e184499
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-08-12 05:53:57 +0000

    In the Tailwind Ala-Carte Folders we want them to look exactly like the free components on TailWindUI

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Features/Application/Components/SideBar.razor.cs
+++
@@ -1,9 +0,0 @@
-namespace TimeWarp.Blazor.Features.Applications.Components
-{
-  using TimeWarp.Blazor.Features.Bases;
-
-  public partial class SideBar: BaseComponent
-  {
-    protected string NavMenuCssClass => ApplicationState.IsMenuExpanded ? null : "collapse";
-  }
-}
