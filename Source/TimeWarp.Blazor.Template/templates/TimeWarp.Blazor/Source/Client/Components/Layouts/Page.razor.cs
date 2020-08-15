bbea45f6d6f2d040c2ad0bfbaf7eb51810caf7c0
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-08-15 19:38:54 +0000

    Build Login Page

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Components/Layouts/Page.razor.cs
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Components/Layouts/Page.razor.cs
@@ -7,6 +7,8 @@
   {
     [Parameter] public RenderFragment HeaderContent { get; set; }
     [Parameter] public RenderFragment MainContent { get; set; }
+    [Parameter] public bool ShowNavBar { get; set; } = true;
+    [Parameter] public bool ShowFooter { get; set; } = true;
 
     private string Version => ApplicationState.Version;
   }
