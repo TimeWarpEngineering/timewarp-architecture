fae306b5d5dda72bb8fd9db5e85046a276f3beae
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-08-15 07:51:42 +0000

    Remove old components

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Features/Application/ComponentsOld/Footer.razor.cs
+++
@@ -1,9 +0,0 @@
-namespace TimeWarp.Blazor.Features.Applications.Components
-{
-  using TimeWarp.Blazor.Features.Bases;
-
-  public partial class Footer: BaseComponent
-  {
-    protected string Version => ApplicationState.Version;
-  }
-}
