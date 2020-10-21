c910d543d7fc1168d4c15a1e8655aacfbdcb0131
ShadyNagy <info@shadynagy.com>
2020-10-21 16:02:30 +0200

    PC and CC changed.

--- Source/TimeWarp.Blazor.Template/templates/Feature.Endpoint/Server.Tests/__RequestName__RequestValidator_Tests.cs
+++ Source/TimeWarp.Blazor.Template/templates/Feature.Endpoint/Server.Tests/__RequestName__RequestValidator_Tests.cs
@@ -1,4 +1,4 @@
-﻿namespace __RequestName__PC__RequestValidator_
+﻿namespace __RequestName__PascalCase__RequestValidator_
 {
   using FluentAssertions;
   using FluentValidation.Results;
@@ -7,30 +7,30 @@
 
   public class Validate_Should
   {
-    private __RequestName__PC__RequestValidator __RequestName__PC__RequestValidator;
+    private __RequestName__PascalCase__RequestValidator __RequestName__PascalCase__RequestValidator;
 
     public Validate_Should()
     {
-      __RequestName__PC__RequestValidator = new __RequestName__PC__RequestValidator();
+      __RequestName__PascalCase__RequestValidator = new __RequestName__PascalCase__RequestValidator();
     }
 
     public void Be_Valid()
     {
-      var __RequestName__CC__Request = new __RequestName__PC__Request
+      var __RequestName__CamelCase__Request = new __RequestName__PascalCase__Request
       {
         // Set Valid values here
         // #TODO
         SampleProperty = "sample"
       };
 
-      ValidationResult validationResult = __RequestName__PC__RequestValidator.TestValidate(__RequestName__CC__Request);
+      ValidationResult validationResult = __RequestName__PascalCase__RequestValidator.TestValidate(__RequestName__CamelCase__Request);
 
       validationResult.IsValid.Should().BeTrue();
     }
 
     // #TODO Rename thie test and add tests for all validation rules
     public void Have_error_when_SampleProperty_is_empty() => __RequestName__RequestValidator
-      .ShouldHaveValidationErrorFor(a__RequestName__PC__Request => a__RequestName__PC__Request.SampleProperty, string.Empty);
+      .ShouldHaveValidationErrorFor(a__RequestName__PascalCase__Request => a__RequestName__PascalCase__Request.SampleProperty, string.Empty);
 
   }
 }
