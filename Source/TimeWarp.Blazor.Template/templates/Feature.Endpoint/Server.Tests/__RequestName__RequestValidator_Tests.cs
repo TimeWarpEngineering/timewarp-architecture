08a4c8988d1021ac7fb0bf8ad5d91af2f608b9cd
ShadyNagy <info@shadynagy.com>
2020-10-21 15:51:22 +0200

    template.json in Feature.Endpoint changed to support camelCase and PascalCase.

--- Source/TimeWarp.Blazor.Template/templates/Feature.Endpoint/Server.Tests/__RequestName__RequestValidator_Tests.cs
+++ Source/TimeWarp.Blazor.Template/templates/Feature.Endpoint/Server.Tests/__RequestName__RequestValidator_Tests.cs
@@ -1,4 +1,4 @@
-﻿namespace __RequestName__RequestValidator_
+﻿namespace __RequestName__PC__RequestValidator_
 {
   using FluentAssertions;
   using FluentValidation.Results;
@@ -7,30 +7,30 @@
 
   public class Validate_Should
   {
-    private __RequestName__RequestValidator __RequestName__RequestValidator;
+    private __RequestName__PC__RequestValidator __RequestName__PC__RequestValidator;
 
     public Validate_Should()
     {
-      __RequestName__RequestValidator = new __RequestName__RequestValidator();
+      __RequestName__PC__RequestValidator = new __RequestName__PC__RequestValidator();
     }
 
     public void Be_Valid()
     {
-      var __RequestName__Request = new __RequestName__Request
+      var __RequestName__CC__Request = new __RequestName__PC__Request
       {
         // Set Valid values here
         // #TODO
         SampleProperty = "sample"
       };
 
-      ValidationResult validationResult = __RequestName__RequestValidator.TestValidate(__RequestName__Request);
+      ValidationResult validationResult = __RequestName__PC__RequestValidator.TestValidate(__RequestName__CC__Request);
 
       validationResult.IsValid.Should().BeTrue();
     }
 
     // #TODO Rename thie test and add tests for all validation rules
     public void Have_error_when_SampleProperty_is_empty() => __RequestName__RequestValidator
-      .ShouldHaveValidationErrorFor(a__RequestName__Request => a__RequestName__Request.SampleProperty, string.Empty);
+      .ShouldHaveValidationErrorFor(a__RequestName__PC__Request => a__RequestName__PC__Request.SampleProperty, string.Empty);
 
   }
 }
