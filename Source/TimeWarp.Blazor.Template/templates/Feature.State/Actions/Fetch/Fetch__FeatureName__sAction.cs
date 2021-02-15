a9941c69abf1662274edf7db48743f2485023a23
Mike Yoshino <yoshinotsubagi@gmail.com>
2021-02-15 03:39:51 +0000

    Add State Template

--- Source/TimeWarp.Blazor.Template/templates/Feature.State/Actions/Fetch/Fetch__FeatureName__sAction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace __RootNamespace__.Features.__FeatureName__s
-{
-  using __RootNamespace__.Features.Bases;
-
-  internal partial class __FeatureName__State
-  {
-    internal class Fetch__FeatureName__sAction : BaseAction { }
-  }
-}
--- Source/TimeWarp.Blazor.Template/templates/Feature.State/__FeatureName__State.cs
+++ Source/TimeWarp.Blazor.Template/templates/Feature.State/__FeatureName__State.cs
@@ -1,21 +1,16 @@
-﻿namespace BlazorHosted.Features.__FeatureName__s
+﻿namespace __RootNamespace__.Features.__FeatureName__s
 {
   using BlazorState;
-  using System.Collections.Generic;
 
   public partial class __FeatureName__State : State<__FeatureName__State>
   {
-    public List<__FeatureName__Dto> ___FeatureName__s { get; set; } = null!;
 
-    public IReadOnlyList<__FeatureName__Dto> __FeatureName__s => ___FeatureName__Dtos.AsReadOnly();
-    public __FeatureName__State() 
-    {
-      ___FeatureName__Dtos = new List<__FeatureName__Dto>();
-    }
+    public int SamepleNumber { get; private set; }
+    public __FeatureName__State() {}
 
     public override void Initialize() 
     {
-      ___FeatureName__s.Clear();
+      SamepleNumber = 10;
     }
 
   }
