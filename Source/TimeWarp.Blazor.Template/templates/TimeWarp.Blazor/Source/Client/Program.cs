c97db585402f3dbac8cec60489390a7960f1936f
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-08-08 06:03:22 +0000

    Register the GRPC IHelloService

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Program.cs
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Program.cs
@@ -1,9 +1,12 @@
 namespace TimeWarp.Blazor.Client
 {
   using BlazorState;
-using Grpc.Net.Client;
+  using Grpc.Net.Client;
+  using Grpc.Net.Client.Web;
   using MediatR;
+  using Microsoft.AspNetCore.Components;
   using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+  using Microsoft.Extensions.Configuration;
   using Microsoft.Extensions.DependencyInjection;
   using PeterLeslieMorris.Blazor.Validation;
   using ProtoBuf.Grpc.Client;
@@ -25,9 +28,9 @@ using Grpc.Net.Client;
       (
         (aOptions) =>
         {
-#if ReduxDevToolsEnabled
+          #if ReduxDevToolsEnabled
           aOptions.UseReduxDevToolsBehavior = true;
-#endif
+          #endif
           aOptions.Assemblies =
             new Assembly[]
             {
@@ -35,7 +38,7 @@ using Grpc.Net.Client;
             };
         }
       );
-      
+
       aServiceCollection.AddFormValidation
       (
         aValidationConfiguration => aValidationConfiguration.AddFluentValidation(typeof(Program).Assembly)
@@ -44,18 +47,67 @@ using Grpc.Net.Client;
       aServiceCollection.AddScoped(typeof(IPipelineBehavior<,>), typeof(EventStreamBehavior<,>));
       aServiceCollection.AddScoped<ClientLoader>();
       aServiceCollection.AddScoped<IClientLoaderConfiguration, ClientLoaderConfiguration>();
-      aServiceCollection.AddSingleton<IHelloService>
-      (
-        aServiceProvider =>
-        {
-          GrpcChannel grpcChannel = aServiceProvider.GetRequiredService<GrpcChannel>();
-          return grpcChannel.CreateGrpcService<IHelloService>();
-        }
-      );
+
+      ConfigureGrpc(aServiceCollection);
 
 #if DEBUG
       new ProjectAnlayzer().Analyze();
 #endif
+    }
+
+    private static void ConfigureGrpc(IServiceCollection aServiceCollection)
+    {
+      aServiceCollection.AddSingleton
+      (
+        aServiceProvider =>
+        {
+          // Get the service address from appsettings.json 
+          IConfiguration config = aServiceProvider.GetRequiredService<IConfiguration>();
+          string backendUrl = config["BackendUrl"];
+
+          // If no address is set then fallback to the current webpage URL
+          if (string.IsNullOrEmpty(backendUrl))
+          {
+            NavigationManager navigationManager = aServiceProvider.GetRequiredService<NavigationManager>();
+            backendUrl = navigationManager.BaseUri;
+          }
+
+          Console.WriteLine($"backendUrl:{backendUrl}");
+
+          // Create a channel with a GrpcWebHandler that is addressed to the backend server.
+          //
+          // GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
+          // then GrpcWeb is recommended because it produces smaller messages.
+          var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
+
+          return GrpcChannel.ForAddress
+          (
+            backendUrl,
+            new GrpcChannelOptions
+            {
+              HttpHandler = httpHandler,
+              //CompressionProviders = ...,
+              //Credentials = ...,
+              //DisposeHttpClient = ...,
+              //HttpClient = ...,
+              //LoggerFactory = ...,
+              //MaxReceiveMessageSize = ...,
+              //MaxSendMessageSize = ...,
+              //ThrowOperationCanceledOnCancellation = ...,
+            }
+          );
+        }
+      );
+
+      aServiceCollection.AddSingleton<IHelloService>
+      (
+        aServiceProvider =>
+        {
+          Console.WriteLine("Getting IHelloService");
+          GrpcChannel grpcChannel = aServiceProvider.GetRequiredService<GrpcChannel>();
+          return grpcChannel.CreateGrpcService<IHelloService>();
+        }
+      );
     }
 
     public static Task Main(string[] aArgumentArray)
