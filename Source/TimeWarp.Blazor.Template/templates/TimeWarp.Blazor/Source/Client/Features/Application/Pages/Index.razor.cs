6cbf2acae26d7800b4651c411fc3ef36103b2a83
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-08-08 06:11:51 +0000

    Try to inject the grpc service IHelloService

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Features/Application/Pages/Index.razor.cs
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Client/Features/Application/Pages/Index.razor.cs
@@ -1,11 +1,24 @@
 namespace TimeWarp.Blazor.Pages
 {
+  using Microsoft.AspNetCore.Components;
+  using System.Threading.Tasks;
   using TimeWarp.Blazor.Features.Bases;
+  using TimeWarp.Blazor.Features.Hellos;
 
   public partial class Index : BaseComponent
   {
     private const string RouteTemplate = "/";
 
+    [Inject]
+    public IHelloService HelloService { get; set; }
+
+
     public static string GetRoute() => RouteTemplate;
+
+    public HelloResponse HelloResponse { get; set; }
+    public HelloRequest HelloRequest { get; set; } = new HelloRequest { Name = "Yo" };
+
+    //async Task Submit() =>
+    //  HelloResponse = await HelloService.SayHelloAsync(HelloRequest);
   }
 }
