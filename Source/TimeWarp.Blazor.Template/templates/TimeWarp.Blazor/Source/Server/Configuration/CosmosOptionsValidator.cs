04b92f683f81c16b9521782deac90fd9acad81bd
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-09-09 12:20:26 +0000

    Add Cosmos Options
    This will show an example of using Options and Validators as well as strongly typed Cosmos DB Settings.

---
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Server/Configuration/CosmosOptionsValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace TimeWarp.Blazor.Configuration
+{
+  using FluentValidation;
+
+  public class CosmosOptionsValidator : AbstractValidator<CosmosOptions>
+  {
+    public CosmosOptionsValidator()
+    {
+      RuleFor(aCosmosOptions => aCosmosOptions.EndPoint).NotEmpty();
+      RuleFor(aCosmosOptions => aCosmosOptions.AccessKey).NotEmpty();
+      RuleFor(aCosmosOptions => aCosmosOptions.EndPoint).NotEmpty();
+      RuleFor(aCosmosOptions => aCosmosOptions.EndPoint).NotEmpty();
+    }
+  }
+}
