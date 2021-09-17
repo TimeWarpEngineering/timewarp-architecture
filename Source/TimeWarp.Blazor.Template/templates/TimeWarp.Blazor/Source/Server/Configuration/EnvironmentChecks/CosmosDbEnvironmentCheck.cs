d70c99e8e01e95cfe72864295829b0927836c4b8
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-09-17 15:26:56 +0000

    Add EnvironmentChecks
    I am not sure about these yet.  Env checks are from Oakton and intended to run from command line post install like part of CI/CD pipeline.  They feel very much like Health checks or liveness tests.
    Maybe organized better and united with health check somehow.

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Server/Configuration/EnvironmentChecks/CosmosDbEnvironmentCheck.cs
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Server/Configuration/EnvironmentChecks/CosmosDbEnvironmentCheck.cs
@@ -1,23 +1,54 @@
 ﻿namespace TimeWarp.Blazor.Configuration
 {
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.Extensions.DependencyInjection;
+  using Microsoft.Extensions.Logging;
   using Microsoft.Extensions.Options;
+  using Oakton;
   using System;
+  using System.Net.Http;
+  using System.Threading.Tasks;
+  using TimeWarp.Blazor.Data;
 
   public class CosmosDbEnvironmentCheck
   {
     private readonly CosmosDbOptions CosmosDbOptions;
+    private readonly IServiceProvider ServiceProvider;
+    private readonly ILogger Logger;
 
-    public CosmosDbEnvironmentCheck(IOptions<CosmosDbOptions> aCosmosDbOptionsAccessor)
+    public CosmosDbEnvironmentCheck
+    (
+      IOptions<CosmosDbOptions> aCosmosDbOptionsAccessor,
+      IServiceProvider aServiceProvider,
+      ILogger<CosmosDbEnvironmentCheck> aLogger
+    )
     {
       CosmosDbOptions = aCosmosDbOptionsAccessor.Value;
+      ServiceProvider = aServiceProvider;
+      Logger = aLogger;
     }
 
-    public static string Description => "Can connect to Cosmos DB";
+    public static string Description => "Connecting to Cosmos DB";
 
-    public void Check()
+    public async Task<bool> CheckAsync()
     {
-      ;
-      Console.WriteLine("All good!");
+      Logger.LogInformation($"Start {nameof(SampleEnvironmentCheck)} ");
+      
+      using IServiceScope scope = ServiceProvider.CreateScope();
+
+      CosmosDbContext cosmosDbContext = scope.ServiceProvider.GetRequiredService<CosmosDbContext>();
+
+      try
+      {
+        await cosmosDbContext.Database.GetCosmosClient().ReadAccountAsync().ConfigureAwait(true);
+      }
+      catch (HttpRequestException)
+      {
+        return false;
+      }
+
+      Logger.LogInformation($"Completed {nameof(SampleEnvironmentCheck)} ");
+      return true;
     }
   }
 }
--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Server/Configuration/EnvironmentChecks/SampleEnvironmentCheck.cs
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Server/Configuration/EnvironmentChecks/SampleEnvironmentCheck.cs
@@ -1,15 +1,23 @@
 ﻿namespace TimeWarp.Blazor.Configuration
 {
+  using Microsoft.Extensions.Logging;
   using System;
 
   public class SampleEnvironmentCheck
   {
+    private readonly ILogger Logger;
     public static string Description => "Sample Environment check";
+
+    public SampleEnvironmentCheck(ILogger<SampleEnvironmentCheck> aLogger)
+    {
+      Logger = aLogger;
+    }
 
     public void Check()
     {
-      ;
-      Console.WriteLine("All good!");
+      Logger.LogInformation($"Start {nameof(SampleEnvironmentCheck)} ");
+      // Do something here.Throw exception to cause a failure.
+      Logger.LogInformation($"Completed {nameof(SampleEnvironmentCheck)} ");
     }
   }
 }
