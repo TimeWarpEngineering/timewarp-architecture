4c0364eb92912cd4d37c4e72eeaa169a2952e3f3
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-03-10 07:14:48 +0000

    Add new Api Project
    This is intended to be shared with consumers of the API in particular the Blazor Client can use the same objects and validators

---
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Api/Features/Base/BaseRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeWarp.Architecture.Features;
+
+/// <summary>
+/// Base Request used for all Requests
+/// </summary>
+/// <remarks>
+/// Requests should be mutable reference types. 
+/// </remarks>
+public abstract record BaseRequest : BaseMessage { }
---
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/Api/Features/WeatherForecast/GetList/GetWeatherForecastsRequest.cs
@@ -0,0 +1,18 @@
+//#WeatherForecast #GetWeatherForecasts #Request #Api
+namespace TimeWarp.Architecture.Features.WeatherForecasts;
+
+using MediatR;
+
+public record GetWeatherForecastsRequest : BaseRequest, IApiRequest, IRequest<GetWeatherForecastsResponse>
+{
+  public const string Route = "weatherForecasts";
+
+  /// <summary>
+  /// The Number of days of forecasts to get
+  /// </summary>
+  /// <example>5</example>
+  public int Days { get; set; }
+  public HttpVerb GetHttpVerb() => HttpVerb.Get;
+  public string GetRoute() => $"{Route}?{nameof(Days)}={Days}&{nameof(CorrelationId)}={CorrelationId}";
+
+}
