2dcdd14d8075ef6b98f48a29c09bd1360e2e0248
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-03-11 09:24:35 +0000

    Add GRPC Reflection nuget

--- Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/GrpcServer/Program.cs
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/GrpcServer/Program.cs
@@ -1,17 +1,20 @@
 using TimeWarp.Architecture.GrpcServer.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder? webApplicationBuilder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+// For instructions on how to configure Kestrel and gRPC clients on macOS,
+// visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc();
+webApplicationBuilder.Services.AddGrpc();
+webApplicationBuilder.Services.AddGrpcReflection();
 
-var app = builder.Build();
+WebApplication webApplication = webApplicationBuilder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+webApplication.MapGrpcService<GreeterService>();
+webApplication.MapGrpcReflectionService();
+webApplication.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-app.Run();
+webApplication.Run();
