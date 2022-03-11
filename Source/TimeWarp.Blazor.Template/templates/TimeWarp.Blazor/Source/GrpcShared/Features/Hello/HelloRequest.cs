70bd801624c12da56c939b9e4b1a417f39eb7cfe
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-03-11 09:26:47 +0000

    Add Sample Hello Service to new GRPC server

---
+++ Source/TimeWarp.Blazor.Template/templates/TimeWarp.Blazor/Source/GrpcShared/Features/Hello/HelloRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeWarp.Architecture.Features.Hellos;
+
+using System.Runtime.Serialization;
+
+[DataContract]
+public class HelloRequest
+{
+  [DataMember(Order = 1)]
+  public string Name { get; set; } = string.Empty;
+}
