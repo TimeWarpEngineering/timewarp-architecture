62ddcf56f4166c1b6fafc66bed0697097f38551a
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-03-22 11:57:34 +0000

    A little more cleanup

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Run.ps1
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Run.ps1
@@ -2,7 +2,8 @@ $Env:ASPNETCORE_ENVIRONMENT = "Development"
 Start-CosmosDbEmulator
 
 Push-Location DevOps/Tye
-Start-Process -FilePath "wt" -ArgumentList "tye run --dashboard --watch --logs console"
+# Start-Process -FilePath "wt" -ArgumentList "tye run --dashboard --watch --logs console"
+Start-Process pwsh -argument '-nologo -noprofile -executionpolicy bypass -command tye run --dashboard --logs console'
 # tye run --dashboard --watch --logs console
 Pop-Location
  
