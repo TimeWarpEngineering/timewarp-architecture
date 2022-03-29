48bbf3894503168430ccc4193985c255caa5a7e5
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-03-29 04:39:41 +0000

    Add Common.csproj
    I want an abstraction for Program.  The idea is to have consistent interface like Startup.cs was but in Program.  We need explicit Program class but we need that anyway to do any testing.

---
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Common/ProgramAbstraction.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeWarp.Architecture;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+public interface IProgram
+{
+  public static abstract void ConfigureServices(IServiceCollection aServiceCollection);
+  public static abstract void ConfigureMiddleware(IApplicationBuilder aApplicationBuilder, IServiceProvider aServiceCollection);
+  public static abstract void ConfigureEndpoints(IEndpointRouteBuilder aEndpointRouteBuilder, IServiceProvider aServiceCollection);
+}
