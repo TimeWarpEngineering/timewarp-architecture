2c850b0bfbf3622655266be42e421fc87dcfc570
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-04-07 07:41:36 +0000

    Use the Delegate now that it works
    fix typos
    remove unused code

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/TimeWarp.Testing/TestServerApplication.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/TimeWarp.Testing/TestServerApplication.cs
@@ -1,13 +1,12 @@
-﻿namespace TimeWarp.Architecture.Testing;
+﻿#nullable enable
+namespace TimeWarp.Architecture.Testing;
 
 using MediatR;
 using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using TimeWarp.Architecture.Features;
 
@@ -18,23 +17,22 @@ using TimeWarp.Architecture.Features;
 /// <remarks>This allows for registering a WebApplication as a dependency and DI can fire it up and shut it down. </remarks>
 /// <typeparam name="TProgram"></typeparam>
 [NotTest]
-public abstract class TestServerApplication<TProgram> : IDisposable, IAsyncDisposable, ISender, IWebApiTestService
+public abstract partial class TestServerApplication<TProgram> : IAsyncDisposable, IWebApiTestService, ISender
   where TProgram : IProgram
 {
-  //[Delegate]
+  [Delegate]
   private readonly ISender ScopedSender;
   private IWebApiTestService WebApiTestService { get; }
-  private bool Disposed;
 
   public readonly WebApplicationHost<TProgram> WebApplicationHost;
   public HttpClient HttpClient { get; }
 
-  public TestServerApplication(WebApplicationHost<TProgram> aWebApplication) : base()
+  public TestServerApplication(WebApplicationHost<TProgram> aWebApplicationHost) : base()
   {
-    WebApplicationHost = aWebApplication;
+    WebApplicationHost = aWebApplicationHost;
 
     // ISender Delegate
-    ScopedSender = new ScopedSender(aWebApplication.Host.Services);
+    ScopedSender = new ScopedSender(aWebApplicationHost.ServiceProvider);
 
     HttpClient = new HttpClient
     {
@@ -42,37 +40,16 @@ public abstract class TestServerApplication<TProgram> : IDisposable, IAsyncDispo
     };
 
     var jsonSerializerOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-    IOptions<JsonSerializerOptions> jsonSerializerOptionsAccesor = Options.Create(jsonSerializerOptions);
-    var webApiService = new WebApiService(HttpClient, jsonSerializerOptionsAccesor);
+    IOptions<JsonSerializerOptions> jsonSerializerOptionsAccessor = Options.Create(jsonSerializerOptions);
+    var webApiService = new WebApiService(HttpClient, jsonSerializerOptionsAccessor);
     WebApiTestService = new WebApiTestService(webApiService);
-  }
-
-  public void Dispose()
-  {
-    Dispose(true);
-    GC.SuppressFinalize(this);
   }
 
   public async ValueTask DisposeAsync()
   {
     Console.WriteLine("==== TestApplication.DisposeAsync ====");
     await DisposeAsyncCore().ConfigureAwait(false);
-    Dispose(false);
-#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
     GC.SuppressFinalize(this);
-#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
-  }
-
-  protected virtual void Dispose(bool aIsDisposing)
-  {
-    if (Disposed) return;
-
-    if (aIsDisposing)
-    {
-      WebApplicationHost?.Dispose();
-    }
-
-    Disposed = true;
   }
 
   protected virtual ValueTask DisposeAsyncCore()
@@ -88,29 +65,29 @@ public abstract class TestServerApplication<TProgram> : IDisposable, IAsyncDispo
   public Task<TResponse> GetResponse<TResponse>(IApiRequest aRequest) => WebApiTestService.GetResponse<TResponse>(aRequest);
   #endregion
 
-  #region ISender
-  public Task<TResponse> Send<TResponse>
-  (
-    IRequest<TResponse> aRequest,
-    CancellationToken aCancellationToken = default
-  ) =>
-    ScopedSender.Send(aRequest, aCancellationToken);
+  //#region ISender
+  //public Task<TResponse> Send<TResponse>
+  //(
+  //  IRequest<TResponse> aRequest,
+  //  CancellationToken aCancellationToken = default
+  //) =>
+  //  ScopedSender.Send(aRequest, aCancellationToken);
 
-  public Task<object> Send(object aRequest, CancellationToken aCancellationToken = default) =>
-    ScopedSender.Send(aRequest, aCancellationToken);
+  //public Task<object> Send(object aRequest, System.Threading.CancellationToken aCancellationToken = default) =>
+  //  ScopedSender.Send(aRequest, aCancellationToken);
 
-  public IAsyncEnumerable<TResponse> CreateStream<TResponse>
-  (
-    IStreamRequest<TResponse> aStreamRequest,
-    CancellationToken aCancellationToken = default
-  ) => throw new NotImplementedException();
+  //public IAsyncEnumerable<TResponse> CreateStream<TResponse>
+  //(
+  //  IStreamRequest<TResponse> aStreamRequest,
+  //  CancellationToken aCancellationToken = default
+  //) => throw new NotImplementedException();
 
-  public IAsyncEnumerable<object> CreateStream
-  (
-    object aRequest,
-    CancellationToken aCancellationToken = default
-  ) => throw new NotImplementedException();
+  //public IAsyncEnumerable<object> CreateStream
+  //(
+  //  object aRequest,
+  //  CancellationToken aCancellationToken = default
+  //) => throw new NotImplementedException();
 
-  #endregion
+  //#endregion
 
 }
