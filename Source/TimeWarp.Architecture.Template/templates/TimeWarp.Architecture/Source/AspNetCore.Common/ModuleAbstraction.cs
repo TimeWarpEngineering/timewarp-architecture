d9f034a7c1beaddfb6834654987f737e1148d2b2
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-04-11 07:17:10 +0000

    Adding ModuleAbstraction
    IProgram should probably inherit from IModule? But duplicated for now.

---
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/AspNetCore.Common/ModuleAbstraction.cs
@@ -0,0 +1,15 @@
+﻿namespace TimeWarp.Architecture;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public interface IModule
+{
+  public static abstract void ConfigureConfiguration(ConfigurationManager aConfigurationManager);
+  public static abstract void ConfigureServices(IServiceCollection aServiceCollection, IConfiguration aConfiguration);
+  public static abstract void ConfigureMiddleware(WebApplication aWebApplication, IServiceProvider aServiceCollection, IHostEnvironment aHostEnvironment);
+  public static abstract void ConfigureEndpoints(IEndpointRouteBuilder aEndpointRouteBuilder, IServiceProvider aServiceCollection);
+}
