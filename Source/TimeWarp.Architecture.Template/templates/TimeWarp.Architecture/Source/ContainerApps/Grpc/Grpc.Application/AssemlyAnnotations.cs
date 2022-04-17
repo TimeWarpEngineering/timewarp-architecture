39b491eea04e43dcf8d5dabebdf9cb0a42ac4a9c
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-04-17 07:06:25 +0000

    Add all the gRPC projects
    imports assembly annotations etc boilerplate.
    Some diagrams to visualize the dependencies.

---
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Grpc/Grpc.Application/AssemlyAnnotations.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeWarp.Architecture;
+
+/// <summary>
+/// A class used to reference the assembly
+/// </summary>
+/// <remarks>
+/// When you want to scan items in assembly you have pass in a class in the assembly.
+/// Refactoring by moving things around can sometimes cause issues.
+/// If you want a specific Assembly you can reference this class.
+/// </remarks>
+
+public class Grpc_Application_Assembly { }
