31eb8a27f5d2a2cbf577a61feb6065d7b530fbc0
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-06-04 18:27:46 +0000

    Add constructor for required params.

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Domain/Entities/Profile/Profile.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Domain/Entities/Profile/Profile.cs
@@ -2,6 +2,14 @@
 
 public class Profile : BaseEntity
 {
+  public Profile(string displayName, string language, string region, string theme)
+  {
+    DisplayName = displayName;
+    Language = language;
+    Region = region;
+    Theme = theme;
+  }
+
   public string DisplayName { get; set; }
   public string Language { get; set; }
   public bool Notifications { get; set; }
