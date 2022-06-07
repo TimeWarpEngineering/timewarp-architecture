4bd08754ea0aa7a01b364bb83d4ac835937a0f3d
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2022-06-07 15:15:12 +0000

    Clean up CommonServerModule

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/Common/Common.Server/CommonServerModule.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/Common/Common.Server/CommonServerModule.cs
@@ -6,22 +6,16 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 public class CommonServerModule : IAspNetModule
 {
-  public static void ConfigureConfiguration(ConfigurationManager aConfigurationManager) => throw new NotImplementedException();
-  public static void ConfigureEndpoints(IEndpointRouteBuilder aEndpointRouteBuilder, IServiceProvider aServiceCollection) => throw new NotImplementedException();
-  public static void ConfigureMiddleware(WebApplication aWebApplication, IServiceProvider aServiceCollection, IHostEnvironment aHostEnvironment) => throw new NotImplementedException();
+  public static void ConfigureConfiguration(ConfigurationManager aConfigurationManager) { }
+  public static void ConfigureEndpoints(IEndpointRouteBuilder aEndpointRouteBuilder, IServiceProvider aServiceCollection) { }
+  public static void ConfigureMiddleware(WebApplication aWebApplication, IServiceProvider aServiceCollection, IHostEnvironment aHostEnvironment) { }
   public static void ConfigureServices(IServiceCollection aServiceCollection, IConfiguration aConfiguration)
   {
     ValidatorOptions.Global.DisplayNameResolver =
       (aType, aMemberInfo, aLambdaExpression) =>
         aType != null && aMemberInfo != null ? $"{aType.Name}:{aMemberInfo.Name}" : null;
-        throw new NotImplementedException();
   }
 }
