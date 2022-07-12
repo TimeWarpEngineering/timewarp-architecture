49807a5311abe82ddb687eaab58778936955f499
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-07-12 19:56:57 +0000

    Update tailwind to 3.0

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.TypeScript/tailwind.config.js
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.TypeScript/tailwind.config.js
@@ -1,33 +1,49 @@
 // tailwind.config.js
 
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  purge: {
-    content: ["../Web.Spa/**/*.razor", "../**/*.html"],
-    options: {
-      whitelist: [""],
-    },
-  },
-  darkmode: false,
+  content: [
+    "../Web.Spa/**/*.razor",
+    "../Web.Spa/**/*.razor.cs",
+    "../**/*.html"
+  ],
+  safelist: [
+    {
+      pattern: /bg-(primary|secondary|accent|danger|warning|positive|gray)-(50|100|200|300|400|500|600|700|800|900)/,
+    }
+  ],
   theme: {
     extend: {
       colors: {
-        primary: defaultTheme.colors.blue,
-        secondary: defaultTheme.colors.gray,
-        accent: defaultTheme.colors.teal,
-        danger: defaultTheme.colors.red,
-        warning: defaultTheme.colors.yellow,
-        positive: defaultTheme.colors.green,
+        primary: {
+          DEFAULT: colors.blue,
+          dark: colors.blue
+        },
+        secondary: {
+          DEFAULT: colors.gray,
+          dark: colors.gray
+        },
+        accent: {
+          DEFAULT: colors.teal,
+          dark: colors.teal,
+        },
+        danger: colors.red,
+        warning: colors.amber,
+        positive:colors.emerald,
       },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  variants: {
-    borderWidth: ["responsive", "hover"],
-    textColor: ["responsive", "hover", "focus", "visited"],
-  },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp')
+  ],
 };
