55abe50345e271d554d2322286f551de7cd381a6
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-08-23 04:05:04 +0000

    adding runforever tests so I can debug servers that start during the test process

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/Web.Server.Integration.Tests/Features/Test/ConventionTests/WebTestServerApplicationTests.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/Web.Server.Integration.Tests/Features/Test/ConventionTests/WebTestServerApplicationTests.cs
@@ -20,4 +20,11 @@ public class Should
   /// This will test that the injected WebTestServerApplication can be created and disposed.
   /// </summary>
   public void Start_Without_Exception() => true.Should().BeTrue();
+
+  //[Skip("This test runs forever to allow me to manually test if servers are running properly.  Normally needs to be skipped as it will never completed")]
+  public async Task RunForever()
+  {
+    await Task.Delay(int.MaxValue);
+    Console.WriteLine("done");
+  }
 }
