83c446c8d47eb5331071ceaa117a87c9dd8ef59f
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-11-16 11:59:56 +0000

    remove scripts

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Scripts/BuildDependencyDiagram.ps1
+++
@@ -1,9 +0,0 @@
-Push-Location $PSScriptRoot
-
-try {   
-  dotnet dg --solutionFile ../TimeWarp.Architecture.sln -o ..\Documentation\Developer\Reference\dependencies-with-nuget.puml --includeNugetPackages
-  dotnet dg --solutionFile ../TimeWarp.Architecture.sln -o ..\Documentation\Developer\Reference\dependencies.puml
-}
-finally {
-  Pop-Location
-}
