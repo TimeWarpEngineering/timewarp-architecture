00ce74770267283dc470206aaf0484197b95a40c
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-11-16 11:21:40 +0000

    removing more

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Server/Features/Analytics/FeatureAnnotations.cs
+++
@@ -1,6 +0,0 @@
-namespace TimeWarp.Architecture.Features.Analytics;
-
-public static class FeatureAnnotations
-{
-  public const string FeatureGroup = "Analytics";
-}
--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Server/Features/Hello/FeatureAnnotations.cs
+++
@@ -1,6 +0,0 @@
-namespace TimeWarp.Architecture.Features.Hello;
-
-public static class FeatureAnnotations
-{
-  public const string FeatureGroup = "Hello";
-}
