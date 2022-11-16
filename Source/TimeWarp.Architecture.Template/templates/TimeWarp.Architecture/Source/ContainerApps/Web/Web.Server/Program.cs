574a798d86db2778d4649defb3da471438e463be
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-11-16 06:41:00 +0000

    remove SwaggerGen and UI

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Server/Program.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Server/Program.cs
@@ -36,15 +36,9 @@ using TimeWarp.Architecture.Web.Infrastructure;
 
 public class Program : IAspNetProgram
 {
-  const string SwaggerVersion = "v1";
-  const string SwaggerApiTitle = $"TimeWarp.Architecture Web.Server API {SwaggerVersion}";
-  const string SwaggerBasePath = "api/web-server";
-  const string SwaggerEndpoint = $"/swagger/{SwaggerVersion}/swagger.json";
-
   public static Task<int> Main(string[] aArgumentArray)
   {
     WebApplicationBuilder builder = WebApplication.CreateBuilder(aArgumentArray);
-
 
     ConfigureConfiguration(builder.Configuration);
     ConfigureServices(builder.Services, builder.Configuration);
@@ -108,15 +102,6 @@ public class Program : IAspNetProgram
         typeof(Web_Server_Assembly).GetTypeInfo().Assembly,
         typeof(Web_Application_Assembly).GetTypeInfo().Assembly
       );
-
-    CommonServerModule
-      .AddSwaggerGen
-      (
-        aServiceCollection,
-        SwaggerVersion,
-        SwaggerApiTitle,
-        new Type[] { typeof(Web_Server_Assembly), typeof(Web_Contracts_Assembly) }
-      );
   }
 
   public static void ConfigureMiddleware(WebApplication aWebApplication)
@@ -132,8 +117,6 @@ public class Program : IAspNetProgram
       aWebApplication.UseDeveloperExceptionPage();
       aWebApplication.UseWebAssemblyDebugging();
     }
-
-    CommonServerModule.UseSwaggerUi(aWebApplication, SwaggerBasePath, SwaggerEndpoint, SwaggerApiTitle);
 
     aWebApplication.UseResponseCompression();
     aWebApplication.UseBlazorFrameworkFiles();
