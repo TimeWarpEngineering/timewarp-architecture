211fadacdceee942f249650bcba7f0dc5ede2cdb
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-11-16 10:16:42 +0000

    remove lint and prettier

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Application/Pipeline/TrackProcessingAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TimeWarp.Architecture.Features.Applications;
-
-using System;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-public class TrackProcessingAttribute : Attribute { }
