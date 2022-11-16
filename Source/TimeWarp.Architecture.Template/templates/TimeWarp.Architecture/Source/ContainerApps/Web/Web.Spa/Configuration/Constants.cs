0d792ab0a49dd340f4d3814774f7d912a7d1c74f
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-11-16 09:37:28 +0000

    remove more noise same status

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Configuration/Constants.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TimeWarp.Architecture.Configuration;
-public static class Constants
-{
-  public const string ApiServiceName = "api-server";
-  public const string WebServiceName = "web-server";
-}
