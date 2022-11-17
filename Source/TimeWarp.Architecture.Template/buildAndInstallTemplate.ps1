729fb3d03557f6d59a1c24986e6032b657d97ac8
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-11-17 16:23:26 +0000

    use latest dotnet new syntax

--- Source/TimeWarp.Architecture.Template/buildAndInstallTemplate.ps1
+++ Source/TimeWarp.Architecture.Template/buildAndInstallTemplate.ps1
@@ -1,4 +1,4 @@
-$Version = '1.2.3'
+$Version = '10.2.3'
 dotnet pack .\TimeWarp.Architecture.csproj -o . /p:PackageVersion=$Version
-dotnet new -u TimeWarp.Architecture
-dotnet new -i TimeWarp.Architecture.$Version.nupkg
+dotnet new uninstall TimeWarp.Architecture
+dotnet new install TimeWarp.Architecture.$Version.nupkg
