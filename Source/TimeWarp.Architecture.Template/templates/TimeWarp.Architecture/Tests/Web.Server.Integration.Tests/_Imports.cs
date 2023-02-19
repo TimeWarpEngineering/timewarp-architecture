0ac5e7b60575bf0ab492a976d167c866dbb50188
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-02-20 00:06:33 +0700

    All tests pass

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/Web.Server.Integration.Tests/_Imports.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/Web.Server.Integration.Tests/_Imports.cs
@@ -1,1 +1,5 @@
 ﻿// global using xyz;
+global using FluentAssertions;
+global using System.Threading.Tasks;
+global using TimeWarp.Architecture.Features.Analytics;
+global using TimeWarp.Architecture.Testing;
