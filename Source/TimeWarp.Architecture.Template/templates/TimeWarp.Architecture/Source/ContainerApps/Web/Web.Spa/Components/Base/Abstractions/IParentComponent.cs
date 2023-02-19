0f190d0b53756e8f053683bb8065795e079e8f42
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-02-19 21:48:10 +0700

    remove unessary usings

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Base/Abstractions/IParentComponent.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Base/Abstractions/IParentComponent.cs
@@ -1,6 +1,5 @@
 namespace TimeWarp.Architecture.Components;
 
-using Microsoft.AspNetCore.Components;
 public interface IParentComponent
 {
   [Parameter] public RenderFragment ChildContent { get; set; }
