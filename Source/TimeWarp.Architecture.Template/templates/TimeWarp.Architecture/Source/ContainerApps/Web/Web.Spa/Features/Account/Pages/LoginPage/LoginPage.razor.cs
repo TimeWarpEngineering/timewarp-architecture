d5918827ce09a3a1bdefff6ea8ab8b765bb68c04
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-03-04 15:00:11 +0700

    Remove Routetemplate stuff we are goign to use the mixin

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Account/Pages/LoginPage/LoginPage.razor.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Account/Pages/LoginPage/LoginPage.razor.cs
@@ -2,7 +2,5 @@ namespace TimeWarp.Architecture.Pages;
 
 public partial class LoginPage : BaseComponent
 {
-  private const string RouteTemplate = "/Login";
-
-  public static string GetRoute() => RouteTemplate;
+  
 }
