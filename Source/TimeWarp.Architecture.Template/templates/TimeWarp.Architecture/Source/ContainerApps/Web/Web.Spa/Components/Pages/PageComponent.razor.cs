560fc4bfb12382e14b3c9bd4e89a4f4dd1536c59
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-03-13 11:35:47 +0700

    Add ModalContent to MainPage

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Pages/PageComponent.razor.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Pages/PageComponent.razor.cs
@@ -4,9 +4,11 @@ public partial class PageComponent : BaseComponent
 {
   [Parameter] public RenderFragment HeaderContent { get; set; }
   [Parameter] public RenderFragment MainContent { get; set; }
+  [Parameter] public RenderFragment ModalContent { get; set; }
   [Parameter] public RenderFragment SiteFooterContent { get; set; }
   [Parameter] public bool ShowNavBar { get; set; } = true;
   [Parameter] public bool ShowFooter { get; set; } = true;
 
+  protected string ActiveModalId => ApplicationState.ActiveModalId;
   private string Version => ApplicationState.Version;
 }
