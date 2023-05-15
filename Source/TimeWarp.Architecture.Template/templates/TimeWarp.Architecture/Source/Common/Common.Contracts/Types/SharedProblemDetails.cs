81065f847dda958c50d1416050375db0885aebe9
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-05-15 21:28:08 +0700

    The actual ProblemDetails object is in the wrong lib so to transfer the object we use this copy of it.

---
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/Common/Common.Contracts/Types/SharedProblemDetails.cs
@@ -0,0 +1,27 @@
+﻿namespace TimeWarp.Architecture.Types;
+
+public sealed class SharedProblemDetails
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-5)]
+    public string? Type { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-4)]
+    public string? Title { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-3)]
+    public int? Status { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-2)]
+    public string? Detail { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-1)]
+    public string? Instance { get; set; }
+
+    [JsonExtensionData]
+    public IDictionary<string, object?> Extensions { get; set; } = new Dictionary<string, object?>(StringComparer.Ordinal);
+}
