a861551f046420f77d0cdc96675ad519fac902d7
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-06-12 01:07:13 +0700

    ToggleMenu Restructure

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Application/Actions/ApplicationState.ToggleMenu.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Application/Actions/ApplicationState.ToggleMenu.cs
@@ -2,15 +2,18 @@ namespace TimeWarp.Architecture.Features.Applications;
 
 internal partial class ApplicationState
 {
-  internal record ToggleMenuAction : BaseAction { }
-  internal class ToggleMenuHandler : BaseHandler<ToggleMenuAction>
+  public static class ToggleMenu
   {
-    public ToggleMenuHandler(IStore store) : base(store) { }
-
-    public override Task Handle(ToggleMenuAction action, CancellationToken cancellationToken)
+    internal record Action : BaseAction { }
+    internal class Handler : BaseHandler<Action>
     {
-      ApplicationState.IsMenuExpanded = !ApplicationState.IsMenuExpanded;
-      return Task.CompletedTask;
+      public Handler(IStore store) : base(store) { }
+
+      public override Task Handle(Action action, CancellationToken cancellationToken)
+      {
+        ApplicationState.IsMenuExpanded = !ApplicationState.IsMenuExpanded;
+        return Task.CompletedTask;
+      }
     }
   }
 }
