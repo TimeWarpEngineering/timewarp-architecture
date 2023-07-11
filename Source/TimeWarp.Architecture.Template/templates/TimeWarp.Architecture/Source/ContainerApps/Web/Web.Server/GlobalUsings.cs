28a7aebec168220e786445dbb22df542722bf067
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-07-12 00:00:15 +0700

    Delete Automapper CreateMap Going with Mapperly for this

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Server/GlobalUsings.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Server/GlobalUsings.cs
@@ -40,7 +40,6 @@ global using TimeWarp.Architecture.CorsPolicies;
 global using TimeWarp.Architecture.Data;
 global using TimeWarp.Architecture.Features.Chat;
 global using TimeWarp.Architecture.Hubs;
-global using TimeWarp.Architecture.Infrastructure;
 global using TimeWarp.Architecture.Persistence;
 global using TimeWarp.Architecture.Types;
 global using TimeWarp.Architecture.Web.Infrastructure;
