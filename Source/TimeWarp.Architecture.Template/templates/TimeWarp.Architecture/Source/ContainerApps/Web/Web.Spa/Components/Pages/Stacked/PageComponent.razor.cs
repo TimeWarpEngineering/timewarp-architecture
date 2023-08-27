3f556261ba383e46c32ceabcd41b20f9407b8973
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-08-27 08:11:24 +0700

    Move PageComponent into Stacked (We will add another Sidebar later)
    Rename SiteFooterContent to  CustomSiteFooterContent to imply there is a default.
    
    Use ShowFooter to control entire display of footer.
    Remove SideNavigationContent because it was never used.

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Pages/Stacked/PageComponent.razor.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Pages/Stacked/PageComponent.razor.cs
@@ -5,8 +5,7 @@ public partial class PageComponent : BaseComponent
   [Parameter] public RenderFragment HeaderContent { get; set; }
   [Parameter] public RenderFragment MainContent { get; set; }
   [Parameter] public RenderFragment ModalContent { get; set; }
-  [Parameter] public RenderFragment SiteFooterContent { get; set; }
-  [Parameter] public RenderFragment SideNavigationContent { get; set; }
+  [Parameter] public RenderFragment CustomSiteFooterContent { get; set; }
   [Parameter] public bool ShowNavBar { get; set; } = true;
   [Parameter] public bool ShowFooter { get; set; } = true;
 
