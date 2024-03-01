f18dbb77e6d8f5cf63b0f5b182ae850638926a33
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-01 23:31:57 +0700

    enable nullable and set the fragments to nullable

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Pages/Stacked/StackedPage.razor.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Pages/Stacked/StackedPage.razor.cs
@@ -1,11 +1,12 @@
+#nullable enable
 namespace TimeWarp.Architecture.Components;
 
 public partial class StackedPage : BaseComponent
 {
-  [Parameter] public RenderFragment HeaderContent { get; set; }
-  [Parameter] public RenderFragment MainContent { get; set; }
-  [Parameter] public RenderFragment ModalContent { get; set; }
-  [Parameter] public RenderFragment CustomSiteFooterContent { get; set; }
+  [Parameter] public RenderFragment? HeaderContent { get; set; }
+  [Parameter] public RenderFragment? MainContent { get; set; }
+  [Parameter] public RenderFragment? ModalContent { get; set; }
+  [Parameter] public RenderFragment? CustomSiteFooterContent { get; set; }
   [Parameter] public bool ShowNavBar { get; set; } = true;
   [Parameter] public bool ShowFooter { get; set; } = true;
 
