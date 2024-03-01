e3a98793e6448d57b09bb6ac8a4b192d6b803b35
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-01 23:34:52 +0700

    Fixed type

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/Web.Spa.Integration.Tests/Infrastructure/SpaTestConvention.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/Web.Spa.Integration.Tests/Infrastructure/SpaTestConvention.cs
@@ -6,8 +6,6 @@ class SpaTestConvention : TimeWarpTestingConvention
   private static void ConfigureAdditionalServicesCallback(ServiceCollection serviceCollection)
   {
     serviceCollection.AddSingleton<SpaTestApplication<YarpTestServerApplication, Yarp.Server.Program>>(); ;
-    // One would configure their Appplication Objects here as well as any other test services
+    // One would configure their Application Objects here as well as any other test services
   }
-
-  
 }
