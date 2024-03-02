d2261ee624e21986f2968d04df19b0fe803bc05e
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-02 14:10:05 +0700

    Support nullable.

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Forms/InputSelectNumber.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Components/Forms/InputSelectNumber.cs
@@ -2,7 +2,7 @@ namespace TimeWarp.Architecture.Components;
 
 public class InputSelectNumber<T> : InputSelect<T>
 {
-  protected override bool TryParseValueFromString(string aValue, out T aResult, out string aValidationErrorMessage)
+  protected override bool TryParseValueFromString(string? aValue, [MaybeNullWhen(false)] out T aResult, [NotNullWhen(false)] out string? aValidationErrorMessage)
   {
     if (typeof(T) == typeof(int))
     {
