0c6df04a2f241b4358fc779a7455dba67d1408f6
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-06 08:17:06 +0700

    remove line-clamp

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.TypeScript/tailwind.config.js
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.TypeScript/tailwind.config.js
@@ -45,6 +45,5 @@ module.exports = {
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/line-clamp"),
   ],
 };
