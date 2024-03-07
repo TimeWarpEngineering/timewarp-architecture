15378feb7f688382d51f3520be67da7ff0deae81
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-07 15:00:21 +0700

    Add Title

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Debugger/Pages/ServicesPage.razor.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Debugger/Pages/ServicesPage.razor.cs
@@ -3,16 +3,17 @@ namespace TimeWarp.Architecture.Pages;
 [Page("/Services")]
 public partial class ServicesPage
 {
+  public static string Title => "Services";
+
   [Inject]
   private IServiceCollection ServiceCollection { get; set; } = null!;
 
-  private List<ServiceDescriptor> IPipelineBehaviors => FilterServices(typeof(IPipelineBehavior<,>));
-  private List<ServiceDescriptor> IRequestPreProcessors => FilterServices(typeof(IRequestPreProcessor<>));
-  private List<ServiceDescriptor> IRequestPostProcessors => FilterServices(typeof(IRequestPostProcessor<,>));
-  private List<ServiceDescriptor> IStreamPipelineBehaviors => FilterServices(typeof(IStreamPipelineBehavior<,>));
+  private List<ServiceDescriptor> PipelineBehaviors => FilterServices(typeof(IPipelineBehavior<,>));
+  private List<ServiceDescriptor> RequestPreProcessors => FilterServices(typeof(IRequestPreProcessor<>));
+  private List<ServiceDescriptor> RequestPostProcessors => FilterServices(typeof(IRequestPostProcessor<,>));
+  private List<ServiceDescriptor> StreamPipelineBehaviors => FilterServices(typeof(IStreamPipelineBehavior<,>));
 
-  private List<ServiceDescriptor> FilterServices(Type serviceType)
-  {
-    return ServiceCollection.Where(s => s.ServiceType == serviceType || s.ServiceType.GetInterfaces().Contains(serviceType)).ToList();
-  }
+  private List<ServiceDescriptor> FilterServices(Type serviceType) =>
+    ServiceCollection.Where
+      (s => s.ServiceType == serviceType || s.ServiceType.GetInterfaces().Contains(serviceType)).ToList();
 }
--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/EventStream/Pages/EventStreamPage.razor.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/EventStream/Pages/EventStreamPage.razor.cs
@@ -2,4 +2,7 @@ namespace TimeWarp.Architecture.Pages;
 
 [UsedImplicitly]
 [Page("/EventStream")]
-public partial class EventStreamPage : BaseComponent;
+public partial class EventStreamPage : BaseComponent
+{
+    public static string Title => "Event Stream";
+}
