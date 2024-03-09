aed7919b7af4b56e22e8405a7c25631b1e2204d3
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-09 09:56:23 +0700

    Add ActionContent with a default Close button

---
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Application/Components/ModalContainer.razor.cs
@@ -0,0 +1,32 @@
+namespace TimeWarp.Architecture.Components;
+
+public partial class ModalContainer
+{
+  [CascadingParameter, EditorRequired] private ModalController Parent { get; set; } = default!;
+  [Parameter, EditorRequired] public RenderFragment MainContent { get; set; } = default!;
+  [Parameter] public RenderFragment ActionContent { get; set; } = default!;
+  [Parameter, EditorRequired] public string ModalId { get; set; } = default!;
+  [Parameter] public EventCallback OnActivate { get; set; }
+  private bool IsActive => Parent.ActiveModalId == ModalId;
+  private Task CloseModal() => Send(new ApplicationState.CloseModal.Action());
+
+  protected override void OnInitialized()
+  {
+    if (Parent == null)
+      throw new ArgumentNullException
+      (
+      nameof(Parent),
+      $"{nameof(ModalContainer)} must exist within a {nameof(ModalController)} Component"
+      );
+
+    if (!OnActivate.HasDelegate)
+      throw new ArgumentNullException
+      (
+      nameof(OnActivate),
+      $"{nameof(OnActivate)} is required"
+      );
+
+    base.OnInitialized();
+    Parent.AddModal(this);
+  }
+}
