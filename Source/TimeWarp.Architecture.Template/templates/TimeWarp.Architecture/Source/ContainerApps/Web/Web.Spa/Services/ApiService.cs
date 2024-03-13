bac8d71a51b561b7e990c0a5742a1d31d0302337
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-03-13 11:33:32 +0700

    Add ApiService and MockApiService Implementations

---
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Services/ApiService.cs
@@ -0,0 +1,8 @@
+﻿namespace TimeWarp.Architecture.Services;
+
+[UsedImplicitly]
+public sealed class ApiService
+(
+  IHttpClientFactory httpClientFactory,
+  IOptions<JsonSerializerOptions> options
+) : WebApiService(httpClientFactory.CreateClient(Constants.ApiServiceName), options);
