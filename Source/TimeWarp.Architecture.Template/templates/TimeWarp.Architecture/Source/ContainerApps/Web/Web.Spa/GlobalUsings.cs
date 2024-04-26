268a7b5c649e4d7286520334cb1f8a2024d59be5
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-04-26 09:36:22 +0700

    Update GlobalUsings.cs

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/GlobalUsings.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/GlobalUsings.cs
@@ -1,21 +1,21 @@
 ﻿global using BlazorComponentUtilities;
+global using BlazorState;
 global using Blazored.LocalStorage;
 global using Blazored.SessionStorage;
-global using BlazorState;
-global using TimeWarp.Features.JavaScriptInterop;
-global using TimeWarp.Features.Routing;
-global using TimeWarp.Features.ReduxDevTools;
 global using Dawn;
 global using FluentValidation;
-global using MediatR;
-global using MediatR.Pipeline;
-global using Grpc.Net.Client;
 global using Grpc.Net.Client.Web;
+global using Grpc.Net.Client;
 global using JetBrains.Annotations;
-global using Microsoft.AspNetCore.Components;
+global using MediatR.Pipeline;
+global using MediatR;
+global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Components.Forms;
 global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.WebAssembly.Authentication.Internal;
 global using Microsoft.AspNetCore.SignalR.Client;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +23,10 @@ global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 global using Microsoft.JSInterop;
 global using Morris.Blazor.Validation;
+global using OneOf;
+global using TimeWarp.Features.JavaScriptInterop;
+global using TimeWarp.Features.ReduxDevTools;
+global using TimeWarp.Features.Routing;
 
 #if(grpc)
 global using ProtoBuf.Grpc.Client;
@@ -33,7 +37,9 @@ global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
 global using System.Net.Http;
 global using System.Reflection;
+global using System.Security.Claims;
 global using System.Text.Json;
+global using System.Text.RegularExpressions;
 global using System.Threading.Tasks;
 
 // Solution usings
@@ -41,7 +47,10 @@ global using TimeWarp.Architecture.Components;
 global using TimeWarp.Architecture.Configuration;
 global using TimeWarp.Architecture.Extensions;
 global using TimeWarp.Architecture.Features;
+global using TimeWarp.Architecture.Features.Admin.Roles;
 global using TimeWarp.Architecture.Features.Applications;
+global using TimeWarp.Architecture.Features.Authentication;
+global using TimeWarp.Architecture.Features.Authorization;
 global using TimeWarp.Architecture.Features.Chat;
 global using TimeWarp.Architecture.Features.EventStreams;
 global using TimeWarp.Architecture.Features.Notifications;
@@ -52,6 +61,7 @@ global using TimeWarp.Architecture.Pipeline.NotificationPostProcessor;
 global using TimeWarp.Architecture.Services;
 global using TimeWarp.Architecture.Types;
 global using TimeWarp.Features.ActionTracking;
+global using static TimeWarp.Architecture.AuthorizationConstants;
 
 
 #if(counter)
