d59f682326faeb6ef5b776357a25380318ee0387
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-04-29 00:16:29 +0700

    Support for Application Modules

---
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Authorization/CustomRequirements/ModuleRequirementHandler.cs
@@ -0,0 +1,19 @@
+namespace TimeWarp.Architecture.CustomRequirements;
+
+[UsedImplicitly]
+public class ModuleRequirementHandler
+(
+  IStore Store
+) : AuthorizationHandler<ModuleRequirement>
+{
+  private AuthorizationState AuthorizationState => Store.GetState<AuthorizationState>();
+
+  protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ModuleRequirement requirement)
+  {
+    bool hasModule = AuthorizationState.Modules != null && AuthorizationState.Modules.Contains(requirement.RequiredModule);
+
+    if (hasModule) context.Succeed(requirement);
+
+    return Task.CompletedTask;
+  }
+}
