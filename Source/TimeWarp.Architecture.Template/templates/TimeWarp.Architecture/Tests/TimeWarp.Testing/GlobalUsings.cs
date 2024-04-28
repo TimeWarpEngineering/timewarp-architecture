2b7ff75d359127e0d23323ff1dc57eb7f445a25c
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-04-29 00:08:09 +0700

    Update GlobalUsings.cs

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/TimeWarp.Testing/GlobalUsings.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Tests/TimeWarp.Testing/GlobalUsings.cs
@@ -3,19 +3,23 @@ global using FluentAssertions;
 global using MediatR;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 global using Microsoft.JSInterop;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Net;
 global using System.Net.Http;
+global using System.Reflection;
 global using System.Text.Json;
 
 // Solution usings
 global using TimeWarp.Architecture.Features;
+global using TimeWarp.Architecture.Services;
 global using TimeWarp.Architecture.Types;
 global using TimeWarp.Fixie;
 
