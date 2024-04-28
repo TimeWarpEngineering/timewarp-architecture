218f7d8e0acced092bfb21ce84a51b7acc062f98
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-04-29 01:49:02 +0700

    Generated files

---
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Api/Api.Contracts/Generated/net8.0/Morris.Moxy/Morris.Moxy.RoslynIncrementalGenerator/RouteMixin.MixinAttribute.Moxy.g.cs
@@ -0,0 +1,20 @@
+﻿// Generated from ..\..\..\Common\Common.Contracts\Mixins\RouteMixin.mixin at 2024-04-28 18:25:48 UTC
+namespace TimeWarp.Architecture
+{
+    
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct, AllowMultiple = true)]
+    internal class RouteMixinAttribute : System.Attribute
+    {
+        public string RouteTemplate { get; set; }
+        public TimeWarp.Architecture.Features.HttpVerb HttpVerb { get; set; }
+        
+        public RouteMixinAttribute(
+            string RouteTemplate,
+            TimeWarp.Architecture.Features.HttpVerb HttpVerb)
+        {
+            this.RouteTemplate = RouteTemplate;
+            this.HttpVerb = HttpVerb;
+        }
+    }
+}
+
