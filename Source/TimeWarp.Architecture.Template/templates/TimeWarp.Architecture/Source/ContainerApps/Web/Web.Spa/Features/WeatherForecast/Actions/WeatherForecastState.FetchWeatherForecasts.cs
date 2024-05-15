03283eaacf7f563b4940b2a54c70367987aa237b
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-15 13:43:48 +0700

    Change order of generics

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/WeatherForecast/Actions/WeatherForecastState.FetchWeatherForecasts.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/WeatherForecast/Actions/WeatherForecastState.FetchWeatherForecasts.cs
@@ -1,5 +1,5 @@
 namespace TimeWarp.Architecture.Features.WeatherForecasts;
-
+using static GetWeatherForecasts;
 internal partial class WeatherForecastsState
 {
   public static class FetchWeatherForecasts
@@ -14,19 +14,18 @@ internal partial class WeatherForecastsState
       }
     }
 
-    [UsedImplicitly]
     internal class Handler
     (
       IStore store,
       IApiServerApiService apiServerApiService,
       ISender sender
-    ) : DefaultFetchHandler<Action,GetWeatherForecasts.Response,GetWeatherForecasts.Query>(store, apiServerApiService, sender)
+    ) : DefaultFetchHandler<Action,Query,Response>(store, apiServerApiService, sender)
     {
-      protected override Task<GetWeatherForecasts.Query?> GetRequest(Action action, CancellationToken cancellationToken)
+      protected override Task<Query?> GetRequest(Action action, CancellationToken cancellationToken)
       {
-        return Task.FromResult<GetWeatherForecasts.Query?>(new GetWeatherForecasts.Query { Days = action.Days ?? 10 });
+        return Task.FromResult<Query?>(new Query { Days = action.Days ?? 10 });
       }
-      protected override Task HandleSuccess(GetWeatherForecasts.Response response, CancellationToken cancellationToken)
+      protected override Task HandleSuccess(Response response, CancellationToken cancellationToken)
       {
         WeatherForecastsState.WeatherForecastList = response.WeatherForecasts.ToList();
         return Task.CompletedTask;
