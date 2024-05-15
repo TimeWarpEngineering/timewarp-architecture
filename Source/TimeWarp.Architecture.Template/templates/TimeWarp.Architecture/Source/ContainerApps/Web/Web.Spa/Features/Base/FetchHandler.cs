eabca034eb96be32d4c61f513b0ccf710fe408ea
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-15 13:44:01 +0700

    Automatic set UserId

--- Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Base/FetchHandler.cs
+++ Source/TimeWarp.Architecture.Template/templates/TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Base/FetchHandler.cs
@@ -1,9 +1,9 @@
 namespace TimeWarp.Architecture.Features;
 
-internal abstract class FetchHandler<TAction, TResponse, TRequest> : BaseHandler<TAction>
+internal abstract class FetchHandler<TAction, TRequest, TResponse> : BaseHandler<TAction>
     where TAction : IBaseAction
-    where TResponse : class
     where TRequest : IApiRequest
+    where TResponse : class
 {
     private readonly AuthenticationStateProvider? AuthenticationStateProvider;
     private readonly IApiService ApiService;
@@ -26,6 +26,7 @@ internal abstract class FetchHandler<TAction, TResponse, TRequest> : BaseHandler
 
         TRequest? request = await GetRequest(action, cancellationToken);
         if (request is null) return; // Skip the action
+        if (request is IAuthApiRequest authRequest) authRequest.UserId = await AuthenticationStateProvider!.GetUserIdAsync();
 
         OneOf<TResponse, SharedProblemDetails> apiResponse =
           await ApiService.GetResponse<TResponse>
