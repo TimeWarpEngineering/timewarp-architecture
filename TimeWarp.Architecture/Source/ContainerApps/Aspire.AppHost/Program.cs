c7696cba00cc5fa91849447553e0dbc01adde12c
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-21 13:38:48 +0700

    Move Aspire projects into folder

--- TimeWarp.Architecture/Source/ContainerApps/Aspire.AppHost/Program.cs
+++
@@ -1,5 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.Api_Server>("api-server");
-
-builder.Build().Run();
---
+++ TimeWarp.Architecture/Source/ContainerApps/Aspire/Aspire.ServiceDefaults/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
+global using Microsoft.Extensions.Logging;
+global using OpenTelemetry;
+global using OpenTelemetry.Metrics;
+global using OpenTelemetry.Trace;
