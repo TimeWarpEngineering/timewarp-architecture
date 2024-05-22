64d854d31e9c4a656845df802f95c266e7975c56
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-23 00:48:37 +0700

    update GlobalUsings.cs

--- TimeWarp.Architecture/Source/ContainerApps/Yarp/GlobalUsings.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Yarp/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using Microsoft.AspNetCore.Routing;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
+global using TimeWarp.Architecture.Extensions;
