6bfcdee8e2a01155c81916e77fd7b48194df7e0e
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-23 00:18:03 +0700

    Add Yarp constant in case.

--- TimeWarp.Architecture/Source/Common/Common.Contracts/Configuration/Constants.cs
+++ TimeWarp.Architecture/Source/Common/Common.Contracts/Configuration/Constants.cs
@@ -4,4 +4,5 @@ public static class Constants
   public const string ApiServiceName = "api-server";
   public const string GrpcServiceName = "grpc-server";
   public const string WebServiceName = "web-server";
+  public const string YarpServiceName = "yarp";
 }
