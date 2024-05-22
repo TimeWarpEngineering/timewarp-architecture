5056d81af6c44816c9cfe6a4927ba4d9b8eb87b1
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-22 11:26:18 +0700

    Add references and WithExternalHttpEndpoints This builds a dependency map.
    see https://github.com/dotnet/aspire/issues/836

--- TimeWarp.Architecture/Source/ContainerApps/Aspire/Aspire.AppHost/Program.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Aspire/Aspire.AppHost/Program.cs
@@ -1,11 +1,18 @@
-var builder = DistributedApplication.CreateBuilder(args);
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Api_Server>("api-server");
+IResourceBuilder<ProjectResource> apiServer =builder.AddProject<Projects.Api_Server>("api-server");
+IResourceBuilder<ProjectResource> grpcServer = builder.AddProject<Projects.Grpc_Server>("grpc-server");
 
-builder.AddProject<Projects.Yarp>("yarp");
+IResourceBuilder<ProjectResource> webServer =
+  builder
+    .AddProject<Projects.Web_Server>("web-server")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiServer)
+    .WithReference(grpcServer);
 
-builder.AddProject<Projects.Web_Server>("web-server");
-
-builder.AddProject<Projects.Grpc_Server>("grpc-server");
+builder.AddProject<Projects.Yarp>("yarp")
+  .WithReference(apiServer)
+  .WithReference(webServer)
+  .WithReference(grpcServer);
 
 builder.Build().Run();
