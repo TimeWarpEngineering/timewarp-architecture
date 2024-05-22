701f5e6f95bad8f962e28778016137d64933adeb
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-23 00:22:13 +0700

    Update GlobalUsings.cs

--- TimeWarp.Architecture/Source/Common/Common.Server/GlobalUsings.cs
+++ TimeWarp.Architecture/Source/Common/Common.Server/GlobalUsings.cs
@@ -1,11 +1,11 @@
-﻿global using Azure.Identity;
+﻿global using Ardalis.GuardClauses;
+global using Azure.Identity;
 global using FluentValidation;
 global using MediatR;
 global using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.AspNetCore.Routing;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
