f42f5a29b3e8a302838ac7a49d6ed57a3e6a26ca
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-10 15:08:03 +0700

    Extract constants into a single file which is Linked by each resource needing it.

---
+++ TimeWarp.Architecture/Source/ContainerApps/Aspire/Aspire.AppHost/Constants.cs
@@ -0,0 +1,12 @@
+namespace TimeWarp.Architecture.Aspire;
+
+internal class Constants
+{
+  public const string CosmosDbResourceName = "cosmos";
+  public const string CosmosDbConnectionStringName = CosmosDbResourceName;
+  public const string CosmosDbDatabaseName = "CosmosDbContext";
+  public const string ApiServerProjectResourceName = "api";
+  public const string WebServerProjectResourceName = "web";
+  public const string GrpcServerProjectResourceName = "grpc";
+  public const string YarpProjectResourceName = "yarp";
+}
--- TimeWarp.Architecture/Source/ContainerApps/Aspire/Aspire.AppHost/Program.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Aspire/Aspire.AppHost/Program.cs
@@ -1,18 +1,24 @@
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-IResourceBuilder<ProjectResource> apiServer =builder.AddProject<Projects.Api_Server>("api-server");
-IResourceBuilder<ProjectResource> grpcServer = builder.AddProject<Projects.Grpc_Server>("grpc-server");
+IResourceBuilder<AzureCosmosDBResource> cosmos = builder.AddAzureCosmosDB(CosmosDbResourceName);
+IResourceBuilder<AzureCosmosDBResource> cosmosdb = cosmos.AddDatabase(CosmosDbDatabaseName);
+#if DEBUG
+cosmosdb.RunAsEmulator();
+#endif
+IResourceBuilder<ProjectResource> apiServer =builder.AddProject<Projects.Api_Server>(ApiServerProjectResourceName);
+IResourceBuilder<ProjectResource> grpcServer = builder.AddProject<Projects.Grpc_Server>(GrpcServerProjectResourceName);
 
 IResourceBuilder<ProjectResource> webServer =
   builder
-    .AddProject<Projects.Web_Server>("web-server")
+    .AddProject<Projects.Web_Server>(WebServerProjectResourceName)
     .WithExternalHttpEndpoints()
+    .WithReference(cosmosdb)
     .WithReference(apiServer)
     .WithReference(grpcServer);
 
     webServer.WithReference(webServer);
 
-builder.AddProject<Projects.Yarp>("yarp")
+builder.AddProject<Projects.Yarp>(YarpProjectResourceName)
   .WithReference(apiServer)
   .WithReference(webServer)
   .WithReference(grpcServer);
