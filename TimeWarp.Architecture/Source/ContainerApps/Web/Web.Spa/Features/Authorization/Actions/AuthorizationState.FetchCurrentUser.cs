6b027882bc317e37891f795c032081620bfed5e2
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-06-29 23:28:11 +0700

    Use the DefaultApiHandler

--- TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Authorization/Actions/AuthorizationState.FetchCurrentUser.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Authorization/Actions/AuthorizationState.FetchCurrentUser.cs
@@ -1,50 +1,36 @@
 namespace TimeWarp.Architecture.Features.Authorization;
+using static GetCurrentUser;
 
 internal partial class AuthorizationState
 {
   public static class FetchCurrentUser
   {
     [TrackAction]
-    public sealed class Action
-    (
-      Guid userId
-    ) : IBaseAction
-    {
-      public Guid UserId { get; } = userId;
-    };
+    public sealed class Action : IBaseAction;
 
-    [UsedImplicitly]
-    public class Handler
-    (
-      IStore store,
-      IWebServerApiService webServerApiService,
-      IPublisher Publisher
-    ) : BaseHandler<Action>(store)
+    public sealed class Handler : DefaultApiHandler<Action,Query, Response>
     {
-      public override async Task Handle(Action action, CancellationToken cancellationToken)
+      public Handler
+      (
+        IStore store,
+        IWebServerApiService webServerApiService,
+        ISender sender
+      ) : base(store, webServerApiService, sender) {}
+
+      protected override Task<Query?> GetRequest(Action action, CancellationToken cancellationToken)
       {
-        var query = new GetCurrentUser.Query
-        {
-          UserId = action.UserId
-        };
+        // Some logic to determine if the request should be skipped (use current state/cache)
 
-        OneOf<GetCurrentUser.Response, SharedProblemDetails> apiResponse =
-          await webServerApiService.GetResponse<GetCurrentUser.Response>
-          (
-            query,
-            cancellationToken
-          );
-
-        apiResponse.Switch
-        (
-          response =>
-          {
-            AuthorizationState.ModulesList = response.Modules;
-            AuthorizationState.RolesList = response.Roles;
-          },
-          problemDetails =>
-            Publisher.Publish(new NotificationState.AddProblemDetails.Action(problemDetails), cancellationToken)
-        );
+        // return UseCache
+        return AuthorizationState.RolesList == null
+          ? Task.FromResult<Query?>(new Query())
+          : Task.FromResult<Query?>(null);
+      }
+      protected override Task HandleSuccess(Response response, CancellationToken cancellationToken)
+      {
+        AuthorizationState.ModulesList = response.Modules;
+        AuthorizationState.RolesList = response.Roles;
+        return Task.CompletedTask;
       }
     }
   }
