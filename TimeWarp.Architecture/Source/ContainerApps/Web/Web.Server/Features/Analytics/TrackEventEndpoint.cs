81b4b27a9720854ac2ad9fa0b440ae8d4e616fdc
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-08-27 19:02:31 +0700

    mild cleanup

--- TimeWarp.Architecture/Source/ContainerApps/Web/Web.Server/Features/Analytics/TrackEventEndpoint.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Web/Web.Server/Features/Analytics/TrackEventEndpoint.cs
@@ -1,6 +1,6 @@
 ﻿namespace TimeWarp.Architecture.Features.Analytics;
 
-using static TimeWarp.Architecture.Features.Analytics.TrackEvent;
+using static TrackEvent;
 public class TrackEventEndpoint : BaseEndpoint<Command, Response>
 {
   /// <summary>
@@ -9,7 +9,7 @@ public class TrackEventEndpoint : BaseEndpoint<Command, Response>
   /// <param name="command"></param>
   /// <returns></returns>
   [HttpPost(Command.Route)]
-  [SwaggerOperation(Tags = new[] { FeatureAnnotations.FeatureGroup })]
+  [SwaggerOperation(Tags = [FeatureAnnotations.FeatureGroup])]
   [ProducesResponseType(typeof(Response), (int)HttpStatusCode.OK)]
   [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
   public Task<IActionResult> Process([FromBody] Command command) => Send(command);
