a610de3165280c14908991e6ff4648e928a56720
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-08-29 22:24:18 +0700

    CssClass is more clear.

--- TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Base/BaseComponent.Css.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Base/BaseComponent.Css.cs
@@ -3,21 +3,21 @@ namespace TimeWarp.Architecture.Features;
 partial class BaseComponent
 {
     [Parameter]
-    public string? Class { get; set; }
+    public string? CssClass { get; set; }
 
     [Parameter(CaptureUnmatchedValues = true)]
     public IReadOnlyDictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
 
-    protected string? BaseClass { get; set; }
+    protected string? BaseCssClass { get; set; }
     protected CssBuilder BaseCssBuilder { get; } = new();
 
     protected override void OnParametersSet()
     {
         base.OnParametersSet();
-        BaseCssBuilder.AddClass(BaseClass);
-        if (string.IsNullOrWhiteSpace(Class)) return;
+        BaseCssBuilder.AddClass(BaseCssClass);
+        if (string.IsNullOrWhiteSpace(CssClass)) return;
 
-        string[] classesToAdd = Class.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string[] classesToAdd = CssClass.Split(separator: ' ', StringSplitOptions.RemoveEmptyEntries);
         foreach (string classToAdd in classesToAdd)
         {
             BaseCssBuilder.AddClass(classToAdd);
@@ -27,7 +27,7 @@ partial class BaseComponent
     // Method to get the class attribute from the Attributes dictionary
     protected string? GetClassFromAttributes()
     {
-        return Attributes.TryGetValue("class", out object? classValue) ?
+        return Attributes.TryGetValue(key: "class", out object? classValue) ?
             classValue as string :
             null;
     }
