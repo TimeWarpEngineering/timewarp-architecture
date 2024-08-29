54fed7986437d45a28729d938b2a3170880d1ff2
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-08-29 21:37:29 +0700

    transitioning HomePage to new TimeWarpPage

--- TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Application/Pages/HomePage.razor.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Web/Web.Spa/Features/Application/Pages/HomePage.razor.cs
@@ -1,28 +1,4 @@
 namespace TimeWarp.Architecture.Pages;
 
-[UsedImplicitly]
 [Page("/")]
-partial class HomePage
-{
-  [Inject]
-  private ILogger<HomePage> Logger { get; set; } = null!;
-
-  private async Task FiveSecondTaskButtonClick() =>
-    await ApplicationState.FiveSecondTask(CancellationToken.None);
-
-  private async Task TwoSecondTaskButtonClick() =>
-    await ApplicationState.TwoSecondTask(CancellationToken.None);
-
-  private async Task ModalButtonClick() =>
-    await Send(new ApplicationState.SetActiveModal.Action(ModalId: AssemblyInfoModal.ModalId));
-
-  protected override void OnInitialized()
-  {
-    base.OnInitialized();
-    Logger.LogDebug("This is a debug message");
-    Logger.LogInformation("This is an info message");
-    Logger.LogWarning("This is a warning message");
-    // Logger.LogError("This is an error message");
-    // Logger.LogCritical("This is a critical message");
-  }
-}
+partial class HomePage;
