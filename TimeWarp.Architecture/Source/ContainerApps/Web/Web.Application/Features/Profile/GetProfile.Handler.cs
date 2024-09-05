1c512839504824d13be060e2e727c5ad41cab006
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-09-05 23:56:16 +0700

    WIP for GetProfile.Handler.cs

--- TimeWarp.Architecture/Source/ContainerApps/Web/Web.Application/Features/Profile/GetProfile.Handler.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Web/Web.Application/Features/Profile/GetProfile.Handler.cs
@@ -1,39 +1,58 @@
-namespace TimeWarp.Architecture.Web.Application.Features.Profile;
-
+namespace TimeWarp.Architecture.Features.Profiles.Application;
+//<SolutionName>.<ContainerName>.Features.<FeatureName>.<Layer>
+//<SolutionName>.Features.<FeatureName>.<Layer>
 using static TimeWarp.Architecture.Features.Profiles.GetProfile;
-
 
 public class GetProfile
 {
   // TODO: Finish implementation
   public class Handler : IRequestHandler<Query, OneOf<Response, SharedProblemDetails>>
   {
+    private readonly ICurrenUserService CurrenUserService;
     private readonly HttpClient HttpClient;
     private readonly ILogger<Handler> Logger;
-    public Handler(HttpClient httpClient, ILogger<Handler> logger)
+    public Handler
+    (
+      ICurrenUserService currenUserService,
+      HttpClient httpClient,
+      ILogger<Handler> logger
+    )
     {
+      CurrenUserService = currenUserService;
       HttpClient = httpClient;
       Logger = logger;
     }
 
     public async Task<OneOf<Response, SharedProblemDetails>> Handle(Query request, CancellationToken cancellationToken)
     {
+      MockResponseFactory<Response> mockResponseFactory = GetMockResponseFactory();
+      Response response = mockResponseFactory(request);
       // https://github.com/kesac/Syllabore
-      var response =
+
+      Guid? userId = CurrenUserService.UserId;
+      if (userId is null) return response;
+      // TODO: Read the Profile from the DB/Repository/Service
+      // The ProfileId will be the UserId.
+
+      response =
         new Response
         (
-          alias:"Use Syllabore",
-          avatar: await GetAvatarDataUri()
+          alias: "Use Syllabore",
+          avatar: await GetAvatarDataUri(userId.Value)
         )
       ;
 
       return response;
     }
 
-    private async Task<string> GetAvatarDataUri()
+
+    // TODO: This will be moved to where we register a user and stored in the DB
+    // By storing in the DB our frontend won't be calling out to mulitavatar.com and leaking infomration.
+    // Only the backend will do it once and then store it.
+    private async Task<string> GetAvatarDataUri(Guid userId)
     {
-      string avatarUrl = "https://multiavatar.com/d8f42d42-f2f8-4332-af82-8ff357f61aa5.svg";
-      // var avatarUrl = $"https://multiavatar.com/{PasskeyId}.svg";
+      // string avatarUrl = "https://api.multiavatar.com/d8f42d42-f2f8-4332-af82-8ff357f61aa5.svg";
+      string avatarUrl = $"https://api.multiavatar.com/{userId}.svg";
       byte[] imageBytes = await HttpClient.GetByteArrayAsync(avatarUrl);
       string base64 = Convert.ToBase64String(imageBytes);
       return $"data:image/svg+xml;base64,{base64}";
