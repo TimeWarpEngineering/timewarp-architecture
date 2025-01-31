e017e8eb296853decb8a7f5edeb9f320932bda9a
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2025-01-31 10:26:00 +0700

    add usings to Globals

--- TimeWarp.Architecture/Source/ContainerApps/Api/Api.Server/GlobalUsings.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Api/Api.Server/GlobalUsings.cs
@@ -1,14 +1,16 @@
-﻿global using FluentValidation;
+﻿global using FastEndpoints;
+global using FluentValidation;
 global using FluentValidation.AspNetCore;
 global using JetBrains.Annotations;
 global using MediatR;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.DependencyInjection;
 global using Oakton;
+global using OneOf;
 global using Swashbuckle.AspNetCore.Annotations;
 global using System.Net;
 global using System.Reflection;
 
 // Solution usings
 global using TimeWarp.Architecture.CorsPolicies;
-
+global using TimeWarp.Architecture.Types;
