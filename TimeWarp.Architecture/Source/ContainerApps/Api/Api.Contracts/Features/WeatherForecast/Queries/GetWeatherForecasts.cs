872292763a4f230a2cf352dd84917c29d38d4b35
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2025-02-07 23:08:45 +0700

    feat: Add FastEndpointSourceGenerator to WeatherForecasts endpoint

--- TimeWarp.Architecture/Source/ContainerApps/Api/Api.Contracts/Features/WeatherForecast/Queries/GetWeatherForecasts.cs
+++ TimeWarp.Architecture/Source/ContainerApps/Api/Api.Contracts/Features/WeatherForecast/Queries/GetWeatherForecasts.cs
@@ -1,75 +1,77 @@
 namespace TimeWarp.Architecture.Features.WeatherForecasts;
 
+using System.Collections.Specialized;
+using TimeWarp.Architecture.Features;
+using TimeWarp.Architecture.Types;
+
+[TimeWarp.Architecture.SourceGenerator.ApiEndpoint]
 public static partial class GetWeatherForecasts
 {
-  [RouteMixin("api/weatherForecasts", HttpVerb.Get)]
-  public sealed partial class Query : IQueryStringRouteProvider, IRequest<OneOf<Response, SharedProblemDetails>>
-  {
-
-    /// <summary>
-    /// The Number of days of forecasts to get
-    /// </summary>
-    /// <example>5</example>
-    public int? Days { get; set; }
-
-    public string GetRouteWithQueryString()
+    public sealed partial class Query : IRequest<OneOf<Response, SharedProblemDetails>>, IQueryStringRouteProvider, IApiRequest
     {
-      var parameters = new NameValueCollection
-      {
-        { nameof(Days), Days?.ToString() }
-      };
+        /// <summary>
+        /// The Number of days of forecasts to get
+        /// </summary>
+        /// <example>5</example>
+        public int? Days { get; set; }
 
-      return $"{this.GetRoute()}?{this.GetQueryString(parameters)}";
+        public string GetRouteWithQueryString()
+        {
+            var parameters = new NameValueCollection
+            {
+                { nameof(Days), Days?.ToString() }
+            };
+
+            return $"{GetRoute()}?{this.GetQueryString(parameters)}";
+        }
+
+        public string GetRoute() => "api/weatherForecasts";
+        
+        public HttpVerb GetHttpVerb() => HttpVerb.Get;
     }
-  }
 
-  public sealed class Response(IEnumerable<WeatherForecastDto> WeatherForecasts) : BaseResponse
-  {
-    public IEnumerable<WeatherForecastDto> WeatherForecasts { get; init; } = WeatherForecasts;
-  }
-
-  /// <summary>
-  /// The weather forecast
-  /// </summary>
-  public sealed class WeatherForecastDto
-  {
-    /// <summary>
-    /// The forecast for this Date
-    /// </summary>
-    /// <example>2020-06-08T12:32:39.9828696+07:00</example>
-    public DateTime Date { get; set; }
-
-    /// <summary>
-    /// Summary of the forecast
-    /// </summary>
-    /// <example>Cool</example>
-    public string Summary { get; set; }
-
-    /// <summary>
-    /// Temperature in Celsius
-    /// </summary>
-    /// <example>24</example>
-    public int TemperatureC { get; set; }
-
-    /// <summary>
-    /// Temperature in Fahrenheit
-    /// </summary>
-    /// <example>75</example>
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public WeatherForecastDto(DateTime date, string summary, int temperatureC)
+    public sealed class Response(IEnumerable<WeatherForecastDto> WeatherForecasts) : BaseResponse
     {
-      Date = date;
-      Summary = summary;
-      TemperatureC = temperatureC;
+        public IEnumerable<WeatherForecastDto> WeatherForecasts { get; init; } = WeatherForecasts;
     }
-  }
 
-  public sealed class Validator : AbstractValidator<Query>
-  {
-    public Validator()
+    /// <summary>
+    /// The weather forecast
+    /// </summary>
+    public sealed class WeatherForecastDto
     {
-      RuleFor(query => query.Days).NotEmpty().GreaterThan(0);
+        /// <summary>
+        /// The forecast for this Date
+        /// </summary>
+        /// <example>2020-06-08T12:32:39.9828696+07:00</example>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Summary of the forecast
+        /// </summary>
+        /// <example>Cool</example>
+        public required string Summary { get; set; }
+
+        /// <summary>
+        /// Temperature in Celsius
+        /// </summary>
+        /// <example>24</example>
+        public int TemperatureC { get; set; }
+
+        /// <summary>
+        /// Temperature in Fahrenheit
+        /// </summary>
+        /// <example>75</example>
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
-  }
+
+    public sealed class Validator : AbstractValidator<Query>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.Days)
+                .GreaterThanOrEqualTo(1)
+                .WithMessage("Days must be greater than or equal to 1");
+        }
+    }
 }
