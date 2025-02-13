cdfcb08015cd3a5df747f4ae7b1aa588d7599a52
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2025-02-13 11:32:12 +0700

    Remove this ApiEndpointAttribute from Common Contracts

--- TimeWarp.Architecture/Source/Common/Common.Contracts/Types/ApiEndpointAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace TimeWarp.Architecture.SourceGenerator;
-
-[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-public sealed class ApiEndpointAttribute : Attribute
-{
-  public Type? EndpointType { get; set; }
-}
