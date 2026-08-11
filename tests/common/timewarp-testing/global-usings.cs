87a1c57a67d98e309ad59bb04862b4494c465545
Steven T. Cramer <steven.cramer@timewarp.enterprises>
2026-08-11 12:00:06 +0700

    chore: restore Purpose regions on global-usings after IDE0005 cleanup (172)

--- tests/common/timewarp-testing/global-usings.cs
+++ tests/common/timewarp-testing/global-usings.cs
@@ -1,3 +1,7 @@
+#region Purpose
+// Project-wide using directives so individual files omit repeated imports.
+#endregion
+
 global using Shouldly;
 global using TimeWarp.Mediator;
 global using Microsoft.Extensions.DependencyInjection;
--- tests/tools/agent-identity-cli-tests/global-usings.cs
+++ tests/tools/agent-identity-cli-tests/global-usings.cs
@@ -1,3 +1,7 @@
+#region Purpose
+// Project-wide using directives so individual files omit repeated imports.
+#endregion
+
 global using System.Buffers.Text;
 global using System.Security.Cryptography;
 global using System.Text;
